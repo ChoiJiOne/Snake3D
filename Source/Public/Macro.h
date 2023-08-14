1b0a37b5e42a492aca37046c39ddc5b81e37f3ed
CHOIJIONE\choij <choijione02@gmail.com>
2023-08-14 19:30:49 +0900

    [GAME] 평가식을 검사하는 어선셜 매크로 추가
    - 디버거가 있을 경우 Assert에 걸리면 브레이크 포인트 걸리도록 설정
    - 디버거가 없을 경우 Assert에 걸리면 크래시 덤프 파일을 생성하도록 설정

--- Source/Public/Macro.h
+++ Source/Public/Macro.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "StringHelper.h"
+
 
 /**
  * @brief 타입의 복사 생성자 및 대입 연산자를 사용하지 못하도록 삭제합니다.
@@ -14,4 +16,44 @@ TYPE(TYPE&&) = delete;\
 TYPE(const TYPE&) = delete;\
 TYPE& operator=(TYPE&&) = delete;\
 TYPE& operator=(const TYPE&) = delete;
+#endif
+
+
+/**
+ * @brief 평가식이 참인지 검사합니다.
+ *
+ * @note
+ * - 디버거가 있으면 브레이크 포인트가 걸립니다.
+ * - 디버거가 없으면 크래시 덤프 파일을 생성합니다.
+ *
+ * @param EXP 검사할 평가식입니다.
+ * @param MESSAGE 평가식을 만족하지 못할 경우 표시할 메시지입니다.
+ */
+#if defined(DEBUG)
+#ifndef ASSERT
+#define ASSERT(EXP, MESSAGE)\
+	{\
+		if (!(EXP))\
+		{\
+			OutputDebugStringA(StringHelper::Format("\nAssertion Checkpoint Failed!\nFILE : %s, LINE : %d, EXP : %s, MESSAGE : %s\n", __FILE__, __LINE__, #EXP, MESSAGE).c_str());\
+			__debugbreak();\
+			ExitProcess(-1);\
+		}\
+	}
+#endif
+#elif defined(RELEASE) 
+#ifndef ASSERT
+#define ASSERT(EXP, MESSAGE)\
+	{\
+		if (!(EXP))\
+		{\
+			OutputDebugStringA(StringHelper::Format("\nAssertion Checkpoint Failed!\nFILE : %s, LINE : %d, EXP : %s, MESSAGE : %s\n", __FILE__, __LINE__, #EXP, MESSAGE).c_str());\
+			__debugbreak();\
+		}\
+	}
+#endif
+#else // defined(SHIPPING)
+#ifndef ASSERT
+#define ASSERT(EXP, MESSAGE)
+#endif
 #endif
