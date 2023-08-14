ce8f6166f0b980ba85fe17e2c426dfb5dd2e8b67
CHOIJIONE\choij <choijione02@gmail.com>
2023-08-14 19:13:15 +0900

    [GAME] 명령행 인자를 파싱하고, 파싱한 인자를 얻는 기능 추가
    - 정규 표현식으로 구현
    - "key=value" 형식과 "-option" 형식만 지원하도록 구현

---
+++ Source/Private/CommandLine.cpp
@@ -0,0 +1,81 @@
+#include <array>
+#include <algorithm>
+#include <regex>
+
+#include "CommandLine.h"
+#include "StringHelper.h"
+
+std::wstring CommandLine::executePath_;
+std::vector<std::wstring> CommandLine::options_;
+std::unordered_map<std::wstring, std::wstring> CommandLine::keyValues_;
+std::unordered_map<CommandLine::EArgumentType, std::wstring> CommandLine::argPatterns_ = {
+	{ CommandLine::EArgumentType::OPTION,   L"^-[a-zA-Z]+$"  },
+	{ CommandLine::EArgumentType::KEYVALUE, L"^[^=]+=[^=]+$" },
+};
+
+void CommandLine::Parse(const std::wstring& cmdLine)
+{
+	std::vector<std::wstring> args = StringHelper::Split(cmdLine, L" ");
+	executePath_ = args[0];
+
+	for (std::size_t index = 1; index < args.size(); ++index)
+	{
+		ParseArg(args[index]);
+	}
+}
+
+bool CommandLine::IsValidOption(const std::wstring& option)
+{
+	return std::find(options_.begin(), options_.end(), option) != options_.end();
+}
+
+bool CommandLine::IsValidKey(const std::wstring& key)
+{
+	return keyValues_.find(key) != keyValues_.end();
+}
+
+std::wstring CommandLine::GetValue(const std::wstring& key)
+{
+	return (IsValidKey(key) ? keyValues_.at(key) : L"");
+}
+
+bool CommandLine::CheckArgPatternMatch(const std::wstring& arg, const std::wstring& pattern)
+{
+	return std::regex_match(arg, std::wregex(pattern));
+}
+
+CommandLine::EArgumentType CommandLine::GetArgType(const std::wstring& arg)
+{
+	EArgumentType argType = EArgumentType::NONE;
+
+	for (const auto& argPattern : argPatterns_)
+	{
+		if (CheckArgPatternMatch(arg, argPattern.second))
+		{
+			argType = argPattern.first;
+			break;
+		}
+	}
+
+	return argType;
+}
+
+void CommandLine::ParseArg(const std::wstring& arg)
+{
+	EArgumentType argType = GetArgType(arg);
+
+	switch (argType)
+	{
+	case EArgumentType::NONE:
+		break;
+
+	case EArgumentType::OPTION:
+		options_.push_back(arg);
+		break;
+
+	case EArgumentType::KEYVALUE:
+		std::vector<std::wstring> keyValue = StringHelper::Split(arg, L"=");
+		keyValues_.insert({ keyValue.front(), keyValue.back() });
+		break;
+	}
+}
--- Source/Public/CommandLine.h
+++ Source/Public/CommandLine.h
@@ -11,4 +11,119 @@
  */
 class CommandLine
 {
+public:
+	/**
+	 * @brief 커맨드 라인을 파싱합니다.
+	 *
+	 * @param cmdLine 파싱할 전체 커맨드 라인 문자열입니다.
+	 */
+	static void Parse(const std::wstring& cmdLine);
+
+
+	/**
+	 * @brief 커맨드 라인 내에서 옵션이 유효한지 확인합니다.
+	 *
+	 * @param option 유효한지 확인할 옵션입니다.
+	 *
+	 * @return 옵션이 유효하다면 true, 그렇지 않으면 false를 반환합니다.
+	 */
+	static bool IsValidOption(const std::wstring& option);
+
+
+	/**
+	 * @brief 커맨드 라인 내에서 키 값이 유효한지 확인합니다.
+	 *
+	 * @param key 유효한지 확인할 키 값입니다.
+	 *
+	 * @return 키 값이 유효하다면 true, 그렇지 않으면 false를 반환합니다.
+	 */
+	static bool IsValidKey(const std::wstring& key);
+
+
+	/**
+	 * @brief 커맨드 라인 내에서 키 값에 대응하는 값을 얻습니다.
+	 *
+	 * @param key 커맨드 라인 내에 유효한 키 값입니다.
+	 *
+	 * @return 키 값에 대응하는 값을 반환합니다. 이때 키 값이 유효하지 않으면 빈 문자열을 반환합니다.
+	 */
+	static std::wstring GetValue(const std::wstring& key);
+
+
+private:
+	/**
+	 * @brief 명령행 인자의 형식입니다.
+	 *
+	 * @note
+	 * - NONE은 지원하지 않는 인자입니다.
+	 * - OPTION은 명령행 인자가 "-<option>" 형식입니다.
+	 * - KEYVALUE는 명령행 인자가 "<key>=<value>" 형식입니다.
+	 */
+	enum class EArgumentType
+	{
+		NONE     = 0x00,
+		OPTION   = 0x01,
+		KEYVALUE = 0x02,
+	};
+
+
+private:
+	/**
+	 * @brief 커맨드 라인 내 인자가 정규 표현식 패턴과 일치하는지 확인합니다.
+	 *
+	 * @param arg 패턴을 검사할 커맨드 라인 내 인자입니다.
+	 * @param pattern 패턴 일치 확인을 위한 정규 표현식 패턴입니다.
+	 *
+	 * @return 커맨드 라인 내 인자가 패턴과 일치하면 true, 그렇지 않으면 false를 반환합니다.
+	 */
+	static bool CheckArgPatternMatch(const std::wstring& arg, const std::wstring& pattern);
+
+
+	/**
+	 * @brief 커맨드 라인 내 인자의 형식을 얻습니다.
+	 *
+	 * @param 형식을 얻을 커맨드 라인 내 인자입니다.
+	 *
+	 * @return 커맨드 라인 내 인자의 형식 열거형을 반환합니다.
+	 */
+	static EArgumentType GetArgType(const std::wstring& arg);
+
+
+	/**
+	 * @brief 명령행 인자를 파싱합니다.
+	 *
+	 * @note 명령행 인자가 유효하지 않으면 아무 동작도 수행하지 않습니다.
+	 *
+	 * @param arg 파싱할 명령행 인자입니다.
+	 */
+	static void ParseArg(const std::wstring& arg);
+
+
+private:
+	/**
+	 * @brief 실행 파일 경로입니다.
+	 */
+	static std::wstring executePath_;
+
+
+	/**
+	 * @brief 커맨드 라인 내의 옵션들입니다.
+	 *
+	 * @note 옵션은 커맨드 라인 내에 "-<optiont>"와 같은 형태입니다.
+	 */
+	static std::vector<std::wstring> options_;
+
+
+	/**
+	 * @brief 커맨드 라인 내의 키-값 쌍입니다.
+	 *
+	 * @note 키-값 쌍은 커맨드 라인 내에 "<key>=<value>"와 같은 형태입니다.
+	 */
+	static std::unordered_map<std::wstring, std::wstring> keyValues_;
+
+
+	/**
+	 * @brief 명령행 인자의 정규 표현식 패턴입니다.
+	 */
+	static std::unordered_map<EArgumentType, std::wstring> argPatterns_;
 };
