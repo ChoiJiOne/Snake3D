80319168fa53b72063030b109150f82323647106
CHOIJIONE\choij <choijione02@gmail.com>
2023-08-14 19:19:32 +0900

    [GAME] 크래시 감지 시 미니 덤프 파일을 생성하는 기능 추가

---
+++ Source/Private/MinidumpWriter.cpp
@@ -0,0 +1,61 @@
+#include "MinidumpWriter.h"
+#include "StringHelper.h"
+
+std::wstring MinidumpWriter::minidumpPath_;
+bool MinidumpWriter::bIsGeneratedDump_ = false;
+
+LONG MinidumpWriter::DetectApplicationCrash(EXCEPTION_POINTERS* exceptionPtr)
+{
+	SYSTEMTIME currentSystemTime;
+	GetLocalTime(&currentSystemTime);
+
+	std::wstring minidumpFilePath = StringHelper::Format(
+		L"%sWindows-%d-%d-%d-%d-%d-%d.dmp",
+		minidumpPath_.c_str(),
+		static_cast<int32_t>(currentSystemTime.wYear),
+		static_cast<int32_t>(currentSystemTime.wMonth),
+		static_cast<int32_t>(currentSystemTime.wDay),
+		static_cast<int32_t>(currentSystemTime.wHour),
+		static_cast<int32_t>(currentSystemTime.wMinute),
+		static_cast<int32_t>(currentSystemTime.wSecond)
+	);
+
+	GenerateMinidumpFile(minidumpFilePath, exceptionPtr);
+	return EXCEPTION_EXECUTE_HANDLER;
+}
+
+void MinidumpWriter::GenerateMinidumpFile(const std::wstring& minidumpFilename, EXCEPTION_POINTERS* exceptionPtr)
+{
+	HANDLE minidumpFileHandle = CreateFileW(
+		minidumpFilename.c_str(),
+		GENERIC_WRITE,
+		FILE_SHARE_WRITE,
+		nullptr,
+		CREATE_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL,
+		nullptr
+	);
+
+	if (minidumpFileHandle == INVALID_HANDLE_VALUE)
+	{
+		bIsGeneratedDump_ = false;
+		return;
+	}
+
+	_MINIDUMP_EXCEPTION_INFORMATION exception;
+	exception.ThreadId = GetCurrentThreadId();
+	exception.ExceptionPointers = exceptionPtr;
+	exception.ClientPointers = FALSE;
+
+	bIsGeneratedDump_ = MiniDumpWriteDump(
+		GetCurrentProcess(),
+		GetCurrentProcessId(),
+		minidumpFileHandle,
+		MiniDumpWithFullMemory,
+		&exception,
+		nullptr,
+		nullptr
+	);
+
+	CloseHandle(minidumpFileHandle);
+}
--- Source/Public/MinidumpWriter.h
+++ Source/Public/MinidumpWriter.h
@@ -13,4 +13,54 @@
 class MinidumpWriter
 {
 public:
+	/**
+	 * @brief 미니 덤프 파일이 저장될 경로를 설정합니다.
+	 *
+	 * @param minidumpPath 설정할 미니 덤프 파일의 경로입니다.
+	 */
+	static void SetMinidumpPath(const std::wstring& minidumpPath)
+	{
+		minidumpPath_ = minidumpPath;
+	}
+
+
+	/**
+	 * @brief 미니 덤프 파일을 생성했는지 확인합니다.
+	 *
+	 * @return 미니 덤프 파일을 생성한 적이 있다면 true, 그렇지 않으면 false를 반환합니다.
+	 */
+	static bool IsGeneratedDump() { return bIsGeneratedDump_; }
+
+
+	/**
+	 * @brief 애플리케이션의 크래시가 감지하고, 크래시 덤프와 콜스택을 기록합니다.
+	 *
+	 * @param exceptionPtr 예외 정보에 대한 포인터 값입니다.
+	 *
+	 * @return SEH의 예외 필터 값인 EXCEPTION_EXECUTE_HANDLER를 반환합니다.
+	 */
+	static LONG WINAPI DetectApplicationCrash(EXCEPTION_POINTERS* exceptionPtr);
+
+
+private:
+	/**
+	 * @brief 미니 덤프 파일을 생성합니다.
+	 *
+	 * @param minidumpFilename 크래시 덤프 파일의 이름입니다.
+	 * @param exceptionPtr 예외 정보에 대한 포인터 값입니다.
+	 */
+	static void GenerateMinidumpFile(const std::wstring& minidumpFilename, EXCEPTION_POINTERS* exceptionPtr);
+
+
+private:
+	/**
+	 * @brief 미니 덤프 파일이 저장될 경로입니다.
+	 */
+	static std::wstring minidumpPath_;
+
+
+	/**
+	 * @brief 덤프 파일 생성의 성공 여부입니다.
+	 */
+	static bool bIsGeneratedDump_;
 };
