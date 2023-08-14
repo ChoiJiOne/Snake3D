6b4bd57423453254056bb6fe9ad688f9ac025feb
CHOIJIONE\choij <choijione02@gmail.com>
2023-08-14 18:55:06 +0900

    [GAME] 아무 동작도 수행하지 않는 빈 메인 함수 추가

---
+++ Source/Private/Application.cpp
@@ -0,0 +1,18 @@
+#include <cstdint>
+#include <windows.h>
+
+
+/**
+ * @brief Windows 애플리케이션의 엔트리 포인트입니다.
+ *
+ * @param hInstance 인스턴스에 대한 핸들 이거나 모듈에 대한 핸들입니다
+ * @param hPrevInstance 16비트 Windows에서 사용되었던 값입니다.
+ * @param cmdLine 유니코드 문자열로 포함된 명령줄 인수입니다.
+ * @param showCmd 기본 애플리케이션 창이 최소화, 최대화 또는 정상적으로 표시되는지 여부를 나타내는 플래그입니다.
+ *
+ * @see https://learn.microsoft.com/en-us/windows/win32/learnwin32/winmain--the-application-entry-point
+ */
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR cmdLine, _In_ int showCmd)
+{
+	return 0;
+}
