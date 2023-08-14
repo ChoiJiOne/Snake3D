c6b509e06aebcc4e9fbe51b9abab757d992dc879
CHOIJIONE\choij <choijione02@gmail.com>
2023-08-14 19:09:31 +0900

    [GAME] 명령행 인자를 파싱하고 관리하는 빈 클래스 추가

---
+++ Source/Public/CommandLine.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+
+/**
+ * @brief 커맨드 라인을 파싱하고 인자를 관리합니다.
+ *
+ * @note 이 클래스의 멤버 변수와 메서드 모두 정적(static) 타입입니다.
+ */
+class CommandLine
+{
+};
