4abf32166e4569ba965c6012a37f955069afdd41
CHOIJIONE\choij <choijione02@gmail.com>
2023-08-14 18:33:01 +0900

    [SCRIPT] Visual Studio 없이 게임을 실행할 수 있는 배치 스크립트 추가

---
+++ Script/RunGame.bat
@@ -0,0 +1,26 @@
+@echo off
+
+SET project=%1
+SET mode=%2
+SET exepath=%project%\\bin\\Win64\\%mode%\\%project%.exe
+SET crash=Crash\\
+SET shader=Source\\Shader\\
+SET content=Content\\
+
+if "%project%" == "" (
+    echo project name is empty...
+    GOTO:EOF
+)
+
+if "%mode%" == "Debug" (
+    echo run %mode% mode game...
+) else if "%mode%" == "Release" (
+    echo run %mode% mode game...
+) else if "%mode%" == "Shipping" (
+    echo run %mode% mode game...
+) else (
+    echo %mode% is illegal mode...
+    GOTO:EOF
+)
+
+start %exepath% Crash=%crash% Shader=%shader% Content=%content% 
