389e5ed63d9c218e4f09070c5c2ad16e6aa15677
CHOIJIONE\choij <choijione02@gmail.com>
2023-08-14 18:59:19 +0900

    [GAME] 특정 문자열을 기준으로 텍스트를 분리하는 기능 추가
    - std::string와 std::wstring 모두 동작하도록 기능 구현

--- Source/Public/StringHelper.h
+++ Source/Public/StringHelper.h
@@ -14,4 +14,54 @@
 class StringHelper
 {
 public:
+	/**
+	 * @brief 특정 문자열을 기준으로 텍스트를 분리합니다.
+	 *
+	 * @param text 분리할 대상 문자열입니다.
+	 * @param delimiter 분리를 위한 기준 문자열입니다.
+	 *
+	 * @return 분리된 문자열을 담고 있는 벡터(std::vector)를 반환합니다.
+	 */
+	static inline std::vector<std::string> Split(const std::string& text, const std::string& delimiter)
+	{
+		std::vector<std::string> tokens;
+		std::size_t position = 0ULL;
+		std::string remain = text;
+
+		while ((position = remain.find(delimiter)) != std::string::npos)
+		{
+			tokens.push_back(remain.substr(0, position));
+			remain.erase(0, position + delimiter.length());
+		}
+
+		tokens.push_back(remain);
+
+		return tokens;
+	}
+
+
+	/**
+	 * @brief 특정 문자열을 기준으로 텍스트를 분리합니다.
+	 *
+	 * @param text 분리할 대상 문자열입니다.
+	 * @param delimiter 분리를 위한 기준 문자열입니다.
+	 *
+	 * @return 분리된 문자열을 담고 있는 벡터(std::vector)입니다.
+	 */
+	static inline std::vector<std::wstring> Split(const std::wstring& text, const std::wstring& delimiter)
+	{
+		std::vector<std::wstring> tokens;
+		std::size_t position = 0ULL;
+		std::wstring remain = text;
+
+		while ((position = remain.find(delimiter)) != std::string::npos)
+		{
+			tokens.push_back(remain.substr(0, position));
+			remain.erase(0, position + delimiter.length());
+		}
+
+		tokens.push_back(remain);
+
+		return tokens;
+	}
 };
