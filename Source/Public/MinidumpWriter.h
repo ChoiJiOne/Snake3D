e178d574cda56624045480508e51639ebe86bb93
CHOIJIONE\choij <choijione02@gmail.com>
2023-08-14 19:17:50 +0900

    [GAME] 미니 덤프 파일을 생성하는 빈 클래스 추가

---
+++ Source/Public/MinidumpWriter.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <windows.h>
+#include <dbghelp.h>
+#include <string>
+
+
+/**
+ * @brief 프로그램 실행 중 발생한 크래시를 기반으로 미니 덤프 파일을 생성합니다.
+ *
+ * @note 이 클래스는 모든 멤버 변수 및 함수가 정적인 정적 클래스입니다.
+ */
+class MinidumpWriter
+{
+public:
+};
