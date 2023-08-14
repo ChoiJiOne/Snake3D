b7d6bf61f78980cd46cf5c19f5fb2a74bebef393
CHOIJIONE\choij <choijione02@gmail.com>
2023-08-14 18:52:40 +0900

    [GAME] 문자열에 대한 핼퍼 기능을 수행하는 빈 클래스 추가

---
+++ Source/Public/StringHelper.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <cstdarg>
+#include <windows.h>
+
+
+/**
+ * @brief 표준 문자열에 대한 헬퍼 기능을 수행합니다.
+ *
+ * @note 이 클래스의 메서드와 멤버 변수는 모두 정적(static) 형식입니다.
+ */
+class StringHelper
+{
+public:
+};
