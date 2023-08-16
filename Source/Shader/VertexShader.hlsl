57bc8038636515dd3c2b747ac9e37fe1be8fd80c
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-08-16 20:46:08 +0900

    [GAME] 3D 정점의 위치를 계산하기 위한 상수 버퍼 추가

--- Source/Shader/VertexShader.hlsl
+++ Source/Shader/VertexShader.hlsl
@@ -10,11 +10,21 @@ struct VS_OUTPUT
     float4 Color : COLOR;
 };
 
+cbuffer cbEveryFrame : register(b0)
+{
+    matrix world;
+    matrix view;
+    matrix projection;
+}
+
 VS_OUTPUT main(VS_INPUT input)
 {
     VS_OUTPUT output;
     
-    output.Position = float4(input.Position, 1.0f);
+    output.Position = mul(float4(input.Position, 1.0f), world);
+    output.Position = mul(output.Position, view);
+    output.Position = mul(output.Position, projection);
+    
     output.Color = input.Color;
     
     return output;
