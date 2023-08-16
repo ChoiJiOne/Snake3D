0500925c2ae7b5fc1e45c2c4eec641ba1764d0a2
CHOIJIONE\choij <choijione02@gmail.com>
2023-08-16 15:20:34 +0900

    [GAME] Direct3D 11을 이용해서 백 버퍼를 초기화하기 위한 최소의 멤버 변수 추가

--- Source/Public/RenderManager.h
+++ Source/Public/RenderManager.h
@@ -52,4 +52,50 @@ private:
 	 * @brief 렌더링 대상이 되는 윈도우입니다.
 	 */
 	Window* renderTargetWindow_ = nullptr;
+
+
+	/**
+	 * @brief 리소스를 만드는 데 사용되는 가상 어뎁터입니다.
+	 * 
+	 * @see https://learn.microsoft.com/ko-kr/windows/win32/api/d3d11/nn-d3d11-id3d11device
+	 */
+	ID3D11Device* device_ = nullptr;
+
+
+	/**
+	 * @brief 렌더링 명령을 생성하는 컨텍스트입니다.
+	 * 
+	 * @see https://learn.microsoft.com/ko-kr/windows/win32/api/d3d11/nn-d3d11-id3d11devicecontext
+	 */
+	ID3D11DeviceContext* context_ = nullptr;
+
+
+	/**
+	 * @brief 렌더링 된 데이터를 출력에 표시하기 전에 저장하기 위한 하나 이상의 표면을 구현합니다.
+	 * 
+	 * @see https://learn.microsoft.com/ko-kr/windows/win32/api/dxgi/nn-dxgi-idxgiswapchain
+	 */
+	IDXGISwapChain* swapChain_ = nullptr;
+	
+
+	/**
+	 * @brief 렌더링 동안 접근 가능한 렌더 타겟 서브 리소스 인터페이스입니다.
+	 * 
+	 * @see https://learn.microsoft.com/en-us/windows/win32/api/d3d11/nn-d3d11-id3d11rendertargetview
+	 */
+	ID3D11RenderTargetView* renderTargetView_ = nullptr;
+
+
+	/**
+	 * @brief 깊이 - 스텐실 버퍼의 텍스처 인터페이스입니다.
+	 * 
+	 * @see https://learn.microsoft.com/ko-kr/windows/win32/api/d3d11/nn-d3d11-id3d11texture2d
+	 */
+	ID3D11Texture2D* depthStencilBuffer_ = nullptr;
+
+
+	/**
+	 * @brief 깊이 - 스텐실 테스트 동안 접근 가능한 텍스처 리소스 인터페이스입니다.
+	 */
+	ID3D11DepthStencilView* depthStencilView_ = nullptr;
 };
