3319a82894ae413da9faba96d5086c4134a99774
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-08-16 18:48:02 +0900

    [GAME] 삼각형 렌더링을 수행하는 기능 추가

--- Source/Private/Application.cpp
+++ Source/Private/Application.cpp
@@ -1,11 +1,18 @@
+#include <array>
 #include <cstdint>
 #include <windows.h>
 #include <d3dcompiler.h>
+#include <DirectXMath.h>
 
 #include "CommandLine.h"
 #include "MinidumpWriter.h"
 #include "RenderManager.h"
 #include "Window.h"
+
+struct Vertex
+{
+	DirectX::XMFLOAT3 Position;
+};
 
 
 /**
@@ -113,6 +120,92 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 	ID3D11PixelShader* pixelShader = nullptr;
 	ID3D11InputLayout* inputLayout = nullptr;
 	ID3D11Buffer* vertexBuffer = nullptr;
+	ID3D11Buffer* indexBuffer = nullptr;
+	ID3DBlob* vsBlob = nullptr;
+	ID3DBlob* psBlob = nullptr;
+
+	std::wstring shaderPath = CommandLine::GetValue(L"Shader");
+	HRESULT_ASSERT(CompileShaderFromFile(shaderPath + L"VertexShader.hlsl", "main", "vs_5_0", &vsBlob), "failed to compile vertex shader...");
+	HRESULT_ASSERT(CompileShaderFromFile(shaderPath + L"PixelShader.hlsl", "main", "ps_5_0", &psBlob), "failed to compile pixel shader...");
+	
+	HRESULT_ASSERT(RenderManager::Get().GetDevice()->CreateVertexShader(
+		vsBlob->GetBufferPointer(),
+		vsBlob->GetBufferSize(),
+		nullptr,
+		&vertexShader
+	), "failed to create vertex shader...");
+
+	HRESULT_ASSERT(RenderManager::Get().GetDevice()->CreatePixelShader(
+		psBlob->GetBufferPointer(),
+		psBlob->GetBufferSize(),
+		nullptr,
+		&pixelShader
+	), "failed to create pixel shader...");
+
+	std::array<D3D11_INPUT_ELEMENT_DESC, 1> layouts =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	};
+
+	HRESULT_ASSERT(RenderManager::Get().GetDevice()->CreateInputLayout(
+		&layouts[0],
+		static_cast<UINT>(layouts.size()),
+		vsBlob->GetBufferPointer(),
+		vsBlob->GetBufferSize(),
+		&inputLayout
+	), "failed to create input layout...");
+	
+	SAFE_RELEASE(vsBlob);
+	SAFE_RELEASE(psBlob);
+
+	std::vector<Vertex> vertices =
+	{
+		Vertex{DirectX::XMFLOAT3(+0.0f, +0.5f, +0.5f)},
+		Vertex{DirectX::XMFLOAT3(+0.5f, -0.5f, +0.5f)},
+		Vertex{DirectX::XMFLOAT3(-0.5f, -0.5f, +0.5f)},
+	};
+	
+	std::vector<uint32_t> indices =
+	{
+		0, 1, 2,
+	};
+
+	D3D11_BUFFER_DESC vertexBufferDesc = {};
+
+	vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	vertexBufferDesc.ByteWidth = sizeof(Vertex) * static_cast<UINT>(vertices.size());
+	vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vertexBufferDesc.CPUAccessFlags = 0;
+	vertexBufferDesc.MiscFlags = 0;
+	vertexBufferDesc.StructureByteStride = 0;
+
+	D3D11_SUBRESOURCE_DATA vertexData;
+	vertexData.pSysMem = reinterpret_cast<const void*>(&vertices[0]);
+	vertexData.SysMemPitch = 0;
+	vertexData.SysMemSlicePitch = 0;
+
+	HRESULT_ASSERT(
+		RenderManager::Get().GetDevice()->CreateBuffer(&vertexBufferDesc, &vertexData, &vertexBuffer), 
+		"failed to create vertex buffer..."
+	);
+
+	D3D11_BUFFER_DESC indexBufferDesc;
+	indexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	indexBufferDesc.ByteWidth = sizeof(uint32_t) * static_cast<UINT>(indices.size());
+	indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	indexBufferDesc.CPUAccessFlags = 0;
+	indexBufferDesc.MiscFlags = 0;
+	indexBufferDesc.StructureByteStride = 0;
+
+	D3D11_SUBRESOURCE_DATA indexData;
+	indexData.pSysMem = reinterpret_cast<const void*>(&indices[0]);
+	indexData.SysMemPitch = 0;
+	indexData.SysMemSlicePitch = 0;
+
+	HRESULT_ASSERT(
+		RenderManager::Get().GetDevice()->CreateBuffer(&indexBufferDesc, &indexData, &indexBuffer),
+		"failed to create index buffer..."
+	);
 	
 	bool bIsDone = false;
 	while (!bIsDone)
@@ -129,11 +222,26 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 			}
 		}
 
-		RenderManager::Get().BeginFrame(1.0f, 0.0f, 0.0f, 1.0f);
+		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 		RenderManager::Get().SetViewport(0.0f, 0.0f, static_cast<float>(width), static_cast<float>(height));
+
+		uint32_t stride = sizeof(Vertex);
+		UINT offset = 0;
+		RenderManager::Get().GetContext()->IASetVertexBuffers(0, 1, &vertexBuffer, &stride, &offset);
+		RenderManager::Get().GetContext()->IASetIndexBuffer(indexBuffer, DXGI_FORMAT_R32_UINT, 0);
+		RenderManager::Get().GetContext()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+		RenderManager::Get().GetContext()->IASetInputLayout(inputLayout);
+
+		RenderManager::Get().GetContext()->VSSetShader(vertexShader, nullptr, 0);
+		RenderManager::Get().GetContext()->PSSetShader(pixelShader, nullptr, 0);
+
+		RenderManager::Get().GetContext()->DrawIndexed(static_cast<UINT>(indices.size()), 0, 0);
+		
 		RenderManager::Get().EndFrame(true);
 	}
 
+	SAFE_RELEASE(indexBuffer);
 	SAFE_RELEASE(vertexBuffer);
 	SAFE_RELEASE(inputLayout);
 	SAFE_RELEASE(pixelShader);
