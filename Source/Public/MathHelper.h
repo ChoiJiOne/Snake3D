ab785b8fa935c782b0f5d7c78e830ca6f4630739
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-08-17 18:04:49 +0900

    [GAME] 외적 연산을 수행하는 메서드의 위치 변경

--- Source/Public/MathHelper.h
+++ Source/Public/MathHelper.h
@@ -291,24 +291,6 @@ public:
 
 
 	/**
-	 * @brief 두 벡터의 외적(Cross) 연산을 수행합니다.
-	 * 
-	 * @param lhs 외적의 왼쪽 피연산자입니다.
-	 * @param rhs 외적의 오른쪽 피연산자입니다.
-	 * 
-	 * @return 두 벡터의 외적 값(벡터 값)을 반환합니다.
-	 */
-	static Vector3f Cross(const Vector3f& lhs, const Vector3f& rhs)
-	{
-		return Vector3f(
-			lhs.y * rhs.z - lhs.z * rhs.y,
-			lhs.z * rhs.x - lhs.x * rhs.z,
-			lhs.x * rhs.y - lhs.y * rhs.x
-		);
-	}
-
-
-	/**
 	 * @brief 두 백터의 내적(Dot) 연산을 수행합니다.
 	 *
 	 * @param lhs 내적의 왼쪽 피연산자입니다.
@@ -319,6 +301,24 @@ public:
 	static float Dot(const Vector4f& lhs, const Vector4f& rhs)
 	{
 		return (lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z + lhs.w * rhs.w);
+	}
+
+
+	/**
+	 * @brief 두 벡터의 외적(Cross) 연산을 수행합니다.
+	 *
+	 * @param lhs 외적의 왼쪽 피연산자입니다.
+	 * @param rhs 외적의 오른쪽 피연산자입니다.
+	 *
+	 * @return 두 벡터의 외적 값(벡터 값)을 반환합니다.
+	 */
+	static Vector3f Cross(const Vector3f& lhs, const Vector3f& rhs)
+	{
+		return Vector3f(
+			lhs.y * rhs.z - lhs.z * rhs.y,
+			lhs.z * rhs.x - lhs.x * rhs.z,
+			lhs.x * rhs.y - lhs.y * rhs.x
+		);
 	}
 
 
