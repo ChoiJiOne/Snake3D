4a27a0de3494062e10459fe43d1d8df99646119c
CHOIJIONE\choij <choijione02@gmail.com>
2023-08-17 11:47:34 +0900

    [GAME] 수학 관련 상수 선언 추가

--- Source/Public/MathHelper.h
+++ Source/Public/MathHelper.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <limits>
+
 #include "Vector/Vector2.h"
 #include "Vector/Vector3.h"
 #include "Vector/Vector4.h"
@@ -7,6 +9,54 @@
 #include "Matrix/Matrix2x2.h"
 #include "Matrix/Matrix3x3.h"
 #include "Matrix/Matrix4x4.h"
+
+
+/**
+ * @brief float 타입의 파이(π) 값입니다.
+ */
+const float PI_F = 3.1415926535F;
+
+
+/**
+ * @brief double 타입의 파이(π) 값입니다.
+ */
+const double PI_D = 3.1415926535897931;
+
+
+/**
+ * @brief float 타입의 타입의 엡실론(ε) 값입니다.
+ */
+const float EPSILON_F = std::numeric_limits<float>::epsilon();
+
+
+/**
+ * @brief double 타입의 엡실론(ε) 값입니다.
+ */
+const double EPSILON_D = std::numeric_limits<double>::epsilon();
+
+
+/**
+ * @brief float 타입의 무한대 값입니다.
+ */
+const float INFINITY_F = std::numeric_limits<float>::infinity();
+
+
+/**
+ * @brief double 타입의 무한대 값입니다.
+ */
+const double INFINITY_D = std::numeric_limits<double>::infinity();
+
+
+/**
+ * @brief float 타입의 음의 무한대 값입니다.
+ */
+const float NINFINITY_F = -std::numeric_limits<float>::infinity();
+
+
+/**
+ * @brief double 타입의 음의 무한대 값입니다.
+ */
+const double NINFINITY_D = -std::numeric_limits<double>::infinity();
 
 
 /**
