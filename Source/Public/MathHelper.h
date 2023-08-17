869b37fa282886ee4a0145afb08c71c7a921db57
CHOIJIONE\choij <choijione02@gmail.com>
2023-08-17 11:33:25 +0900

    [GAME] 수학에 대한 헬퍼 기능을 수행하는 빈 클래스 추가

---
+++ Source/Public/MathHelper.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Vector/Vector2.h"
+#include "Vector/Vector3.h"
+#include "Vector/Vector4.h"
+
+#include "Matrix/Matrix2x2.h"
+#include "Matrix/Matrix3x3.h"
+#include "Matrix/Matrix4x4.h"
+
+
+/**
+ * @brief 수학 관련 헬퍼 기능을 수행합니다.
+ * 
+ * @note 이 클래스의 메서드와 멤버 변수는 모두 정적(static) 형식입니다.
+ */
+class MathHelper
+{
+public:
+};
