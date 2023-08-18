b026064c0f915a54eea375c817dade015095d465
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-08-18 16:56:43 +0900

    [GAME] 행렬과 벡터 자료형의 정의를 이동
    - 백터 자료형의 정의는 MathHelper에서 Vector.h로 이동
    - 행렬 자료형의 정의는 MathHelper에서 Matrix.h로 이동

--- Source/Vector/Vector.h
+++ Source/Vector/Vector.h
@@ -1,5 +1,43 @@
 #pragma once
 
+#include <cstdint>
+
 #include "Vector2.h"
 #include "Vector3.h"
 #include "Vector4.h"
+
+
+/**
+ * @brief int 타입의 2차원 벡터입니다.
+ */
+using Vector2i = Vector2<int32_t>;
+
+
+/**
+ * @brief float 타입의 2차원 벡터입니다.
+ */
+using Vector2f = Vector2<float>;
+
+
+/**
+ * @brief int 타입의 3차원 벡터입니다.
+ */
+using Vector3i = Vector3<int32_t>;
+
+
+/**
+ * @brief float 타입의 3차원 벡터입니다.
+ */
+using Vector3f = Vector3<float>;
+
+
+/**
+ * @brief int 타입의 4차원 벡터입니다.
+ */
+using Vector4i = Vector4<int32_t>;
+
+
+/**
+ * @brief float 타입의 4차원 벡터입니다.
+ */
+using Vector4f = Vector4<float>;
