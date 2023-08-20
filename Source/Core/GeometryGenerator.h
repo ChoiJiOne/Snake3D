4f23c706927549d538021048fa3eac32b00812c1
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-08-20 17:46:11 +0900

    [GAME] 버텍스와 인덱스 목록을 생성하는 빈 클래스 추가

---
+++ Source/Core/GeometryGenerator.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Core/Vertex.h"
+
+
+/**
+ * @brief 버텍스와 인덱스 목록을 생성합니다.
+ * 
+ * @note 이 클래스는 모든 변수 및 멤버 변수가 정적인 정적(static) 클래스입니다.
+ */
+class GeometryGenerator
+{
+public:
+
+};
