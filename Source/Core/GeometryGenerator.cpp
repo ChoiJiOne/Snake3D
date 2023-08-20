30cfa5aa9bf4c501ca4b540f282ac14f3dce4105
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-08-20 18:30:56 +0900

    [GAME] 큐브와 각 평면과 평행한 쿼드의 버텍스와 인덱스를 생성하는 기능 추가

---
+++ Source/Core/GeometryGenerator.cpp
@@ -0,0 +1,148 @@
+#include "GeometryGenerator.h"
+
+void GeometryGenerator::CreateBox(float xsize, float ysize, float zsize, std::vector<VertexPosition>& outVertices, std::vector<uint32_t>& outIndices)
+{
+	outVertices.resize(0);
+	outIndices.resize(0);
+
+	float halfXSize = xsize / 2.0f;
+	float halfYSize = ysize / 2.0f;
+	float halfZSize = zsize / 2.0f;
+
+	outVertices = {
+		VertexPosition(Vector3f(-halfXSize, -halfYSize, +halfZSize)),
+		VertexPosition(Vector3f(+halfXSize, -halfYSize, +halfZSize)),
+		VertexPosition(Vector3f(+halfXSize, +halfYSize, +halfZSize)),
+		VertexPosition(Vector3f(-halfXSize, +halfYSize, +halfZSize)),
+
+		VertexPosition(Vector3f(-halfXSize, -halfYSize, -halfZSize)),
+		VertexPosition(Vector3f(+halfXSize, -halfYSize, -halfZSize)),
+		VertexPosition(Vector3f(+halfXSize, +halfYSize, -halfZSize)),
+		VertexPosition(Vector3f(-halfXSize, +halfYSize, -halfZSize))
+	};
+
+	outIndices = {
+		0, 1, 2,
+		0, 2, 3,
+
+		4, 6, 5,
+		4, 7, 6,
+
+		4, 5, 1,
+		4, 1, 0,
+
+		3, 2, 6,
+		3, 6, 7,
+
+		1, 5, 6,
+		1, 6, 2,
+
+		4, 0, 3,
+		4, 3, 7
+	};
+}
+
+void GeometryGenerator::CreateBox(float xsize, float ysize, float zsize, const Vector4f& color, std::vector<VertexPositionColor>& outVertices, std::vector<uint32_t>& outIndices)
+{
+	outVertices.resize(0);
+	outIndices.resize(0);
+
+	float halfXSize = xsize / 2.0f;
+	float halfYSize = ysize / 2.0f;
+	float halfZSize = zsize / 2.0f;
+
+	outVertices = {
+		VertexPositionColor(Vector3f(-halfXSize, -halfYSize, +halfZSize), color),
+		VertexPositionColor(Vector3f(+halfXSize, -halfYSize, +halfZSize), color),
+		VertexPositionColor(Vector3f(+halfXSize, +halfYSize, +halfZSize), color),
+		VertexPositionColor(Vector3f(-halfXSize, +halfYSize, +halfZSize), color),
+
+		VertexPositionColor(Vector3f(-halfXSize, -halfYSize, -halfZSize), color),
+		VertexPositionColor(Vector3f(+halfXSize, -halfYSize, -halfZSize), color),
+		VertexPositionColor(Vector3f(+halfXSize, +halfYSize, -halfZSize), color),
+		VertexPositionColor(Vector3f(-halfXSize, +halfYSize, -halfZSize), color)
+	};
+
+	outIndices = {
+		0, 1, 2,
+		0, 2, 3,
+
+		4, 6, 5,
+		4, 7, 6,
+
+		4, 5, 1,
+		4, 1, 0,
+
+		3, 2, 6,
+		3, 6, 7,
+
+		1, 5, 6,
+		1, 6, 2,
+
+		4, 0, 3,
+		4, 3, 7
+	};
+}
+
+void GeometryGenerator::CreateQuadXY(float xsize, float ysize, std::vector<VertexPosition>& outVertices, std::vector<uint32_t>& outIndices)
+{
+	outVertices.resize(0);
+	outIndices.resize(0);
+
+	float halfXSize = xsize / 2.0f;
+	float halfYSize = ysize / 2.0f;
+
+	outVertices = {
+		VertexPosition(Vector3f(-halfXSize, -halfYSize, 0.0f)),
+		VertexPosition(Vector3f(-halfXSize, +halfYSize, 0.0f)),
+		VertexPosition(Vector3f(+halfXSize, +halfYSize, 0.0f)),
+		VertexPosition(Vector3f(+halfXSize, -halfYSize, 0.0f)),
+	};
+
+	outIndices = {
+		0, 1, 2,
+		0, 2, 3,
+	};
+}
+
+void GeometryGenerator::CreateQuadYZ(float ysize, float zsize, std::vector<VertexPosition>& outVertices, std::vector<uint32_t>& outIndices)
+{
+	outVertices.resize(0);
+	outIndices.resize(0);
+
+	float halfYSize = ysize / 2.0f;
+	float halfZSize = zsize / 2.0f;
+
+	outVertices = {
+		VertexPosition(Vector3f(0.0f, -halfYSize, -halfZSize)),
+		VertexPosition(Vector3f(0.0f, +halfYSize, -halfZSize)),
+		VertexPosition(Vector3f(0.0f, +halfYSize, +halfZSize)),
+		VertexPosition(Vector3f(0.0f, -halfYSize, +halfZSize)),
+	};
+
+	outIndices = {
+		0, 1, 2,
+		0, 2, 3,
+	};
+}
+
+void GeometryGenerator::CreateQuadXZ(float xsize, float zsize, std::vector<VertexPosition>& outVertices, std::vector<uint32_t>& outIndices)
+{
+	outVertices.resize(0);
+	outIndices.resize(0);
+
+	float halfXSize = xsize / 2.0f;
+	float halfZSize = zsize / 2.0f;
+
+	outVertices = {
+		VertexPosition(Vector3f(-halfXSize, 0.0f, -halfZSize)),
+		VertexPosition(Vector3f(-halfXSize, 0.0f, +halfZSize)),
+		VertexPosition(Vector3f(+halfXSize, 0.0f, +halfZSize)),
+		VertexPosition(Vector3f(+halfXSize, 0.0f, -halfZSize)),
+	};
+
+	outIndices = {
+		0, 1, 2,
+		0, 2, 3,
+	};
+}
--- Source/Core/GeometryGenerator.h
+++ Source/Core/GeometryGenerator.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <vector>
+
 #include "Core/Vertex.h"
+
+#include "Vector/Vector.h"
 
 
 /**
@@ -11,5 +15,60 @@
 class GeometryGenerator
 {
 public:
+	/**
+	 * @brief 위치 좌표가 포함된 정점의 박스를 생성합니다.
+	 *
+	 * @param xsize 박스의 x 축 방향으로의 크기입니다.
+	 * @param ysize 박스의 y 축 방향으로의 크기입니다.
+	 * @param zsize 박스의 z 축 방향으로의 크기입니다.
+	 * @param outVertices 생성된 박스의 정점 목록입니다.
+	 * @param outIndices 생성된 박스 정점의 인덱스 목록입니다.
+	 */
+	static void CreateBox(float xsize, float ysize, float zsize, std::vector<VertexPosition>& outVertices, std::vector<uint32_t>& outIndices);
 
+
+	/**
+	 * @brief 위치 좌표와 색상이 포함된 정점의 박스를 생성합니다.
+	 *
+	 * @param xsize 박스의 x 축 방향으로의 크기입니다.
+	 * @param ysize 박스의 y 축 방향으로의 크기입니다.
+	 * @param zsize 박스의 z 축 방향으로의 크기입니다.
+	 * @param color 박스의 색상입니다.
+	 * @param outVertices 생성된 박스의 정점 목록입니다.
+	 * @param outIndices 생성된 박스 정점의 인덱스 목록입니다.
+	 */
+	static void CreateBox(float xsize, float ysize, float zsize, const Vector4f& color, std::vector<VertexPositionColor>& outVertices, std::vector<uint32_t>& outIndices);
+
+
+	/**
+	 * @brief 위치 좌표가 포함된 XY평면 쿼드를 생성합니다.
+	 *
+	 * @param xsize 쿼드의 x 축 뱡향으로의 크기입니다.
+	 * @param ysize 쿼드의 y 축 방향으로의 크기입니다.
+	 * @param outVertices 생성된 쿼드의 정점 목록입니다.
+	 * @param outIndices 생성된 쿼드의 정점의 인덱스 목록입니다.
+	 */
+	static void CreateQuadXY(float xsize, float ysize, std::vector<VertexPosition>& outVertices, std::vector<uint32_t>& outIndices);
+
+
+	/**
+	 * @brief 위치 좌표가 포함된 YZ평면 쿼드를 생성합니다.
+	 *
+	 * @param ysize 쿼드의 y 축 방향으로의 크기입니다.
+	 * @param zsize 쿼드의 z 축 방향으로의 크기입니다.
+	 * @param outVertices 생성된 쿼드의 정점 목록입니다.
+	 * @param outIndices 생성된 쿼드의 정점의 인덱스 목록입니다.
+	 */
+	static void CreateQuadYZ(float ysize, float zsize, std::vector<VertexPosition>& outVertices, std::vector<uint32_t>& outIndices);
+
+
+	/**
+	 * @brief 위치 좌표가 포함된 XZ평면 쿼드를 생성합니다.
+	 *
+	 * @param xsize 쿼드의 x 축 뱡향으로의 크기입니다.
+	 * @param zsize 쿼드의 z 축 방향으로의 크기입니다.
+	 * @param outVertices 생성된 쿼드의 정점 목록입니다.
+	 * @param outIndices 생성된 쿼드의 정점의 인덱스 목록입니다.
+	 */
+	static void CreateQuadXZ(float xsize, float zsize, std::vector<VertexPosition>& outVertices, std::vector<uint32_t>& outIndices);
 };
