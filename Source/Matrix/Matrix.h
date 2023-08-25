c1064d143b9d1d1420dd3db77fda77e4d4e27b3d
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-08-25 10:51:14 +0900

    [GAME] Matrix 폴더 내 소스 코드의 헤더 선언 정리
    - 폴더 위치를 헤더에 명시하도록 변경

--- Source/Matrix/Matrix.h
+++ Source/Matrix/Matrix.h
@@ -2,9 +2,9 @@
 
 #include <cstdint>
 
-#include "Matrix2x2.h"
-#include "Matrix3x3.h"
-#include "Matrix4x4.h"
+#include "Matrix/Matrix2x2.h"
+#include "Matrix/Matrix3x3.h"
+#include "Matrix/Matrix4x4.h"
 
 
 /**
