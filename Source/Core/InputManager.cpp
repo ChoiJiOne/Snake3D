84302639558a9fa10712fd252ce9f3522bcfce70
CHOIJIONE\choij <choijione02@gmail.com>
2023-08-25 15:48:58 +0900

    [GAME] 윈도우 메시지를 처리하는 정적 메서드가 입력 처리 매니저 내부의 윈도우 메시지 처리 기능을 사용하도록 변경

--- Source/Core/InputManager.cpp
+++ Source/Core/InputManager.cpp
@@ -1,9 +1,20 @@
 #include "InputManager.h"
 
+/**
+ * @brief 윈도우 프로시저를 처리하기 위한 포인터 변수입니다.
+ */
+static InputManager* inputManager = nullptr;
+
+LRESULT InputManager::WindowMessageHandler(HWND windowHandle, uint32_t messageCode, WPARAM wParam, LPARAM lParam)
+{
+	return inputManager->ProcessWindowMessage(windowHandle, messageCode, wParam, lParam);
+}
+
 void InputManager::Initialize()
 {
 	ASSERT(!bIsInitialized_, "already initialize input manager...");
 
+	inputManager = this;
 	bIsInitialized_ = true;
 }
 
@@ -11,10 +22,11 @@ void InputManager::Release()
 {
 	ASSERT(bIsInitialized_, "you have to call Initialize method...");
 
+	inputManager = nullptr;
 	bIsInitialized_ = false;
 }
 
-LRESULT InputManager::WindowMessageHandler(HWND windowHandle, uint32_t messageCode, WPARAM wParam, LPARAM lParam)
+LRESULT InputManager::ProcessWindowMessage(HWND windowHandle, uint32_t messageCode, WPARAM wParam, LPARAM lParam)
 {
 	switch (messageCode)
 	{
