197188b863da00399ff2042250e139dfaf76ed6c
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-08-25 10:13:34 +0900

    [GAME] 삭제 누락된 부분 제거

--- Source/Core/Application.cpp
+++ Source/Core/Application.cpp
@@ -151,8 +151,6 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 
 	RenderManager::Get().Release();
 
-	CoUninitialize();
-
 	SetUnhandledExceptionFilter(topLevelExceptionFilter);
 	return 0;
 }
