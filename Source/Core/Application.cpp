8d5b3bb1978f3c8017aeb799ab2dfbbff0485573
CHOIJIONE\choij <choijione02@gmail.com>
2023-08-27 17:24:40 +0900

    [GAME] 메인 함수 내의 셰이더 관련 코드 삭제

--- Source/Core/Application.cpp
+++ Source/Core/Application.cpp
@@ -86,15 +86,6 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 	model->SetMesh(vertices, indices);
 	model->SetColorMaterial(Vector4f(1.0f, 1.0f, 0.0f, 1.0f));
 
-	ColorMaterialShader colorMaterialShader;
-	colorMaterialShader.Initialize();
-
-	ShapePassShader shapePassShader;
-	shapePassShader.Initialize();
-
-	GlyphPassShader glyphPassShader;
-	glyphPassShader.Initialize();
-
 	TTFont* font = ResourceManager::Get().AddResource<TTFont>("SeoulNamsanEB");
 	font->Initialize("D:\\Snake3D\\Content\\Font\\SeoulNamsanEB.ttf", 32, 127, 32.0f);
 
@@ -114,16 +105,12 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 		RenderManager::Get().SetViewport(0.0f, 0.0f, static_cast<float>(width), static_cast<float>(height));
 
-		colorMaterialShader.Draw(MathHelper::RotationYMatrix(gameTimer.GetTotalSeconds()), &camera, model);
-		glyphPassShader.DrawText2D(font, L"Hello World", Vector2f(0.0f, 0.0f), Vector4f(1.0f, 0.0f, 0.0f, 1.0f));
+		RenderManager::Get().DrawModel3D(MathHelper::RotationYMatrix(gameTimer.GetTotalSeconds()), &camera, model);
+		RenderManager::Get().DrawText2D(font, L"Hello World", Vector2f(0.0f, 0.0f), Vector4f(1.0f, 0.0f, 0.0f, 1.0f));
 		
 		RenderManager::Get().EndFrame(true);
 	}
-
-	glyphPassShader.Release();
-	shapePassShader.Release();
-	colorMaterialShader.Release();
-
+	
 	ResourceManager::Get().Release();
 	RenderManager::Get().Release();
 	AudioManager::Get().Release();
