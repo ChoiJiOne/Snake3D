46cbe05f284b2b120447091955b6c09bdd8be662
CHOIJIONE\choij <choijione02@gmail.com>
2023-08-28 15:16:44 +0900

    [GAME] 버퍼의 내용을 파일에 쓰는 기능 추가
    - std::string와 std::wstring 모두 지원하도록 구현

--- Source/Utils/FileHelper.h
+++ Source/Utils/FileHelper.h
@@ -53,4 +53,44 @@ public:
 		ASSERT(ReadFile(fileHandle, &outBuffer[0], fileSize, &bytesRead, nullptr), "failed read file...");
 		ASSERT(CloseHandle(fileHandle), "failed to close file...");
 	}
+
+
+	/**
+	 * @brief 버퍼를 파일에 씁니다.
+	 *
+	 * @param path 파일의 경로입니다.
+	 * @param buffer 파일에 쓸 버퍼입니다.
+	 * 
+	 * @see https://learn.microsoft.com/ko-kr/windows/win32/api/fileapi/nf-fileapi-createfilea
+	 */
+	static inline void WriteBufferFromFile(const std::string& path, const std::vector<uint8_t>& buffer)
+	{
+		HANDLE fileHandle = CreateFileA(path.c_str(), GENERIC_WRITE, FILE_SHARE_READ, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+		ASSERT((fileHandle != INVALID_HANDLE_VALUE), "failed to create file...");
+
+		DWORD bytesWrite;
+
+		ASSERT(WriteFile(fileHandle, &buffer[0], static_cast<DWORD>(buffer.size()), &bytesWrite, nullptr), "failed to write file...");
+		ASSERT(CloseHandle(fileHandle), "failed to close file...");
+	}
+
+
+	/**
+	 * @brief 버퍼를 파일에 씁니다.
+	 * 
+	 * @param path 파일의 경로입니다.
+	 * @param buffer 파일에 쓸 버퍼입니다.
+	 * 
+	 * @see https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilew
+	 */
+	static inline void WriteBufferFromFile(const std::wstring& path, const std::vector<uint8_t>& buffer)
+	{
+		HANDLE fileHandle = CreateFileW(path.c_str(), GENERIC_WRITE, FILE_SHARE_READ, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+		ASSERT((fileHandle != INVALID_HANDLE_VALUE), "failed to create file...");
+
+		DWORD bytesWrite;
+
+		ASSERT(WriteFile(fileHandle, &buffer[0], static_cast<DWORD>(buffer.size()), &bytesWrite, nullptr), "failed to write file...");
+		ASSERT(CloseHandle(fileHandle), "failed to close file...");
+	}
 };
