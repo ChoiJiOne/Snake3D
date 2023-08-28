3adaf0c4ab611a6a478e78ab65850fca2c4b4d6b
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-08-28 21:59:04 +0900

    [CLIENT/SOURCE] 본격적인 클라이언트 개발을 위해 기존의 기능 테스트용 코드 삭제

--- Client/Source/LaunchApp.cpp
+++ Client/Source/LaunchApp.cpp
@@ -14,51 +14,20 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 {
 	GameEngine::LaunchStartup();
 
-	uint32_t width = 0;
-	uint32_t height = 0;
-	RenderManager::Get().GetRenderTargetWindow()->GetClientSize(width, height);
-	
-	Camera3D camera;
-	camera.Initialzie(
-		Vector3f(0.0f, 10.0f, -10.0f),
-		Vector3f(0.0f, 0.0f, 0.0f),
-		Vector3f(0.0f, 1.0f, 0.0f),
-		PI_F / 4.0f,
-		static_cast<float>(width) / static_cast<float>(height),
-		0.1f,
-		100.0f
-	);
-	
-	std::vector<VertexPosition> vertices;
-	std::vector<uint32_t> indices;
-	GeometryGenerator::CreateQuadXZ(10.0f, 10.0f, vertices, indices);
+	uint32_t clientWidth = 0;
+	uint32_t clientHeight = 0;
+	RenderManager::Get().GetRenderTargetWindow()->GetClientSize(clientWidth, clientHeight);
 
-	Model* model = ResourceManager::Get().AddResource<Model>("QuadXZ");
-	model->SetMesh(vertices, indices);
-	model->SetColorMaterial(Vector4f(1.0f, 1.0f, 0.0f, 1.0f));
-
-	//TTFont* font = ResourceManager::Get().AddResource<TTFont>("SeoulNamsanEB");
-	//font->Initialize("D:\\Work\\Snake3D\\Content\\Font\\SeoulNamsanEB.ttf", 32, 127, 32.0f);
-
-	GameTimer gameTimer;
-	gameTimer.Reset();
-	
 	while (true)
 	{
-		gameTimer.Tick();
 		InputManager::Get().Tick();
-
 		if (InputManager::Get().ShouldCloseWindow())
 		{
 			break;
 		}
 
 		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
-		RenderManager::Get().SetViewport(0.0f, 0.0f, static_cast<float>(width), static_cast<float>(height));
-
-		RenderManager::Get().DrawModel3D(MathHelper::RotationYMatrix(gameTimer.GetTotalSeconds()), &camera, model);
-		//RenderManager::Get().DrawText2D(font, L"Hello World", Vector2f(0.0f, 0.0f), Vector4f(1.0f, 0.0f, 0.0f, 1.0f));
-
+		RenderManager::Get().SetViewport(0.0f, 0.0f, static_cast<float>(clientWidth), static_cast<float>(clientHeight));
 		RenderManager::Get().EndFrame(true);
 	}
 
