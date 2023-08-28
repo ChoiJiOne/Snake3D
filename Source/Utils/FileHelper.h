0a1915d966cdc0077b0de86718093daaebc27c35
CHOIJIONE\choij <choijione02@gmail.com>
2023-08-28 15:22:28 +0900

    [GAME] 파일의 유효성을 검사하는 기능 추가
    - std::string와 std::wstring 모두 지원하도록 구현

--- Source/Utils/FileHelper.h
+++ Source/Utils/FileHelper.h
@@ -125,4 +125,34 @@ public:
 	{
 		return PathIsDirectoryW(directoryPath.c_str());
 	}
+
+
+	/**
+	 * @brief 파일이 유효한지 검사합니다.
+	 * 
+	 * @param filePath 유효한지 검사할 파일 경로입니다.
+	 *
+	 * @return 파일이 유효하다면 true, 그렇지 않으면 false를 반환합니다.
+	 *
+	 * @see https://learn.microsoft.com/en-us/windows/win32/api/shlwapi/nf-shlwapi-pathfileexistsa
+	 */
+	static inline bool IsValidFile(const std::string& filePath)
+	{
+		return PathFileExistsA(filePath.c_str());
+	}
+
+
+	/**
+	 * @brief 파일이 유효한지 검사합니다.
+	 * 
+	 * @param filePath 유효한지 검사할 파일 경로입니다.
+	 *
+	 * @return 파일이 유효하다면 true, 그렇지 않으면 false를 반환합니다.
+	 *
+	 * @see https://learn.microsoft.com/en-us/windows/win32/api/shlwapi/nf-shlwapi-pathfileexistsw
+	 */
+	static inline bool IsValidFile(const std::wstring& filePath)
+	{
+		return PathFileExistsW(filePath.c_str());
+	}
 };
