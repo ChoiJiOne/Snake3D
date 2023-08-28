4dbafe2203cfc5586265bddef6c6472321dd386b
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-08-28 21:02:37 +0900

    [ENGINE/SOURCE] 최상위 예외 처리 필터를 등록/해제하는 기능 추가

--- Engine/Source/Game/GameEngine.cpp
+++ Engine/Source/Game/GameEngine.cpp
@@ -6,12 +6,16 @@ void GameEngine::LaunchStartup()
 {
 	ASSERT(!bIsInitialized_, "already launch game engine startup...");
 
+	MinidumpWriter::RegisterUnhandledExceptionFilter();
+
 	bIsInitialized_ = true;
 }
 
 void GameEngine::LaunchShutdown()
 {
 	ASSERT(bIsInitialized_, "you have to launch game engine startup...");
+
+	MinidumpWriter::UnregisterUnhandledExceptionFilter();
 
 	bIsInitialized_ = false;
 }
