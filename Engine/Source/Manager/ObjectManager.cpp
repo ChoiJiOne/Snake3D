22708cd996c140b726c6c21f78a3b380eda04696
CHOIJIONE\choij <choijione02@gmail.com>
2023-08-30 11:22:50 +0900

    [ENGINE/SOURCE] 오브젝트들을 일괄 업데이트 및 렌더링하는 기능 추가

--- Engine/Source/Manager/ObjectManager.cpp
+++ Engine/Source/Manager/ObjectManager.cpp
@@ -1,5 +1,7 @@
 #include "Manager/ObjectManager.h"
 
+#include <algorithm>
+
 void ObjectManager::Initialize()
 {
 	ASSERT(!bIsInitialized_, "already initialize object manager...");
@@ -26,6 +28,62 @@ void ObjectManager::RemoveGameObject(const std::string& signature)
 {
 	ASSERT((gameObjects_.find(signature) != gameObjects_.end()), "not exist game object signature key...");
 
-	gameObjects_[signature]->Release();
+	IGameObject* object = gameObjects_.at(signature).get();
+
+	for (IGameObject* updateObject : updateObjects_)
+	{
+		if (updateObject == object)
+		{
+			updateObjects_.remove(updateObject);
+			break;
+		}
+	}
+
+	for (IGameObject* renderObject : renderObjects_)
+	{
+		if (renderObject == object)
+		{
+			renderObjects_.remove(renderObject);
+			break;
+		}
+	}
+
+	object->Release();
 	gameObjects_.erase(signature);
+}
+
+void ObjectManager::UpdateAllObjects(float deltaSeconds)
+{
+	if (bNeedsToBeSortedUpdate_)
+	{
+		updateObjects_.sort([](const IGameObject* lhs, const IGameObject* rhs) -> bool
+			{
+				return lhs->GetUpdateOrder() < rhs->GetUpdateOrder();
+			});
+
+		bNeedsToBeSortedUpdate_ = false;
+	}
+
+	for (IGameObject* object : updateObjects_)
+	{
+		object->Update(deltaSeconds);
+	}
+}
+
+void ObjectManager::RenderAllObjects()
+{
+	if (bNeedsToBeSortedRender_)
+	{
+		renderObjects_.sort([](const IGameObject* lhs, const IGameObject* rhs) -> bool
+			{
+				return lhs->GetRenderOrder() < rhs->GetRenderOrder();
+			});
+
+		bNeedsToBeSortedRender_ = false;
+	}
+
+	for (IGameObject* object : renderObjects_)
+	{
+		object->Render();
+	}
 }
--- Engine/Source/Manager/ObjectManager.h
+++ Engine/Source/Manager/ObjectManager.h
@@ -52,6 +52,9 @@ public:
 		std::unique_ptr<TGameObject> object = std::make_unique<TGameObject>();
 		gameObjects_.insert({ signature, std::move(object) });
 
+		updateObjects_.push_back(gameObjects_.at(signature).get());
+		renderObjects_.push_back(gameObjects_.at(signature).get());
+
 		return reinterpret_cast<TGameObject*>(gameObjects_.at(signature).get());
 	}
 
@@ -80,6 +83,36 @@ public:
 	void RemoveGameObject(const std::string& signature);
 
 
+	/**
+	 * @brief 오브젝트들을 일괄 업데이트합니다.
+	 * 
+	 * @param deltaSeconds 델타 시간 값입니다.
+	 */
+	void UpdateAllObjects(float deltaSeconds);
+
+
+	/**
+	 * @brief 오브젝트들을 일괄 렌더링합니다.
+	 */
+	void RenderAllObjects();
+
+
+	/**
+	 * @brief 업데이트할 오브젝트들의 정렬 여부를 설정합니다.
+	 * 
+	 * @param bNeedsToBeSortedUpdate 설정할 업데이트할 오브젝트들의 정렬 여부입니다.
+	 */
+	bool SetNeedsToBeSortedUpdate(bool bNeedsToBeSortedUpdate) { bNeedsToBeSortedUpdate_ = bNeedsToBeSortedUpdate; }
+
+
+	/**
+	 * @brief 렌더링할 오브젝트들의 정렬 여부를 설정합니다.
+	 * 
+	 * @param bNeedsToBeSortedRender 설정할 렌더링할 오브젝트들의 정렬 여부입니다.
+	 */
+	bool SetNeedsToBeSortedRender(bool bNeedsToBeSortedRender) { bNeedsToBeSortedRender_ = bNeedsToBeSortedRender; }
+
+
 private:
 	/**
 	 * @brief 게임 오브젝트를 관리하는 매니저의 디폴트 생성자와 가상 소멸자를 추가합니다.
@@ -92,4 +125,28 @@ private:
 	 * @brief 게임 오브젝트 매니저가 관리하는 오브젝트입니다.
 	 */
 	std::unordered_map<std::string, std::unique_ptr<IGameObject>> gameObjects_;
+
+
+	/**
+	 * @brief 업데이트할 오브젝트들의 포인터 목록의 정렬이 필요한지 확인합니다.
+	 */
+	bool bNeedsToBeSortedUpdate_ = false;
+
+
+	/**
+	 * @brief 업데이트할 오브젝트들의 포인터 목록입니다.
+	 */
+	std::list<IGameObject*> updateObjects_;
+
+
+	/**
+	 * @brief 렌더링할 오브젝트들의 포인터 목록의 정렬이 필요한지 확인합니다.
+	 */
+	bool bNeedsToBeSortedRender_ = false;
+
+
+	/**
+	 * @brief 렌더링할 오브젝트들의 포인터 목록입니다.
+	 */
+	std::list<IGameObject*> renderObjects_;
 };
