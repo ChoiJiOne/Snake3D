c418c97006178cdb59983709b76bc634214a69f4
CHOIJIONE\choij <choijione02@gmail.com>
2023-09-02 10:27:29 +0900

    [GAME] 일괄 업데이트 및 렌더링 기능 삭제

--- Source/Manager/ObjectManager.cpp
+++ Source/Manager/ObjectManager.cpp
@@ -30,60 +30,6 @@ void ObjectManager::RemoveGameObject(const std::string& signature)
 
 	IGameObject* object = gameObjects_.at(signature).get();
 
-	for (IGameObject* updateObject : updateObjects_)
-	{
-		if (updateObject == object)
-		{
-			updateObjects_.remove(updateObject);
-			break;
-		}
-	}
-
-	for (IGameObject* renderObject : renderObjects_)
-	{
-		if (renderObject == object)
-		{
-			renderObjects_.remove(renderObject);
-			break;
-		}
-	}
-
 	object->Release();
 	gameObjects_.erase(signature);
-}
-
-void ObjectManager::UpdateAllObjects(float deltaSeconds)
-{
-	if (bNeedsToBeSortedUpdate_)
-	{
-		updateObjects_.sort([](const IGameObject* lhs, const IGameObject* rhs) -> bool
-			{
-				return lhs->GetUpdateOrder() < rhs->GetUpdateOrder();
-			});
-
-		bNeedsToBeSortedUpdate_ = false;
-	}
-
-	for (IGameObject* object : updateObjects_)
-	{
-		object->Update(deltaSeconds);
-	}
-}
-
-void ObjectManager::RenderAllObjects()
-{
-	if (bNeedsToBeSortedRender_)
-	{
-		renderObjects_.sort([](const IGameObject* lhs, const IGameObject* rhs) -> bool
-			{
-				return lhs->GetRenderOrder() < rhs->GetRenderOrder();
-			});
-
-		bNeedsToBeSortedRender_ = false;
-	}
-
-	for (IGameObject* object : renderObjects_)
-	{
-		object->Render();
-	}
 }
