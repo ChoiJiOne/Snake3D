e52ff6e71ecf237d72076dc97aac78a86dc3bb8a
CHOIJIONE\choij <choijione02@gmail.com>
2023-09-02 09:36:23 +0900

    [SCRIPT] 프로젝트 전체 구조 변경 사항 적용

--- premake5.lua
+++ premake5.lua
@@ -13,43 +13,25 @@ workspace "Snake3D"
     -- 솔루션 경로를 설정합니다.
     location "%{wks.name}"
 
-    -- 엔진 경로입니다.
-    engine="%{wks.location}/../Engine"
+    -- 소스 코드 경로입니다.
+    source="%{wks.location}/../Source"
 
-    -- 엔진의 소스 코드 경로입니다.
-    engineSource="%{engine}/Source"
+    -- 셰이더 파일 경로입니다.
+    shader="%{wks.location}/../HLSL"
 
-    -- 엔진의 설정 파일 경로입니다.
-    engineConfig="%{engine}/Config"
+    -- 스크립트 경로입니다.
+    script="%{wks.location}/../Script"
 
-    -- 엔진의 셰이더 파일 경로입니다.
-    engineShader="%{engine}/HLSL"
-
-    -- 엔진의 스크립트 경로입니다.
-    engineScript="%{engine}/Script"
-
-    -- 엔진의 서드 파티 라이브러리 경로입니다.
-    engineThirdparty="%{engine}/ThirdParty"
-
-    -- 클라이언트 경로입니다.
-    client="%{wks.location}/../Client"
-
-    -- 클라이언트의 소스 코드 경로입니다.
-    clientSource="%{client}/Source"
-
-    -- 클라이언트의 설정 파일 경로입니다.
-    clientConfig="%{client}/Config"
-
-    -- 클라이언트의 리소스 파일 경로입니다.
-    clientResource="%{client}/Resource"
+    -- 서드 파티 라이브러리 경로입니다.
+    thirdparty="%{wks.location}/../ThirdParty"
 
     -- 시작 프로젝트를 설정합니다.
-    startproject "Client"
+    startproject "Snake3D"
 
-    -- 엔진 프로젝트입니다.
-    project "Engine"
+    -- Snake3D 프로젝트입니다.
+    project "Snake3D"
         -- 엔진 프로젝트의 종류를 설정합니다.
-        kind "StaticLib"
+        kind "WindowedApp"
 
         -- 프로그래밍 언어를 설정합니다.
         language "C++"
@@ -59,34 +41,32 @@ workspace "Snake3D"
 
         -- 엔진의 include 경로를 추가합니다.
         includedirs {
-            "%{engineSource}",
-            "%{engineConfig}",
-            "%{engineShader}",
-            "%{engineScript}",
-            "%{engineThirdparty}",
+            "%{source}",
+            "%{shader}",
+            "%{script}",
+            "%{thirdparty}",
         }
 
         -- 엔진의 file 경로를 추가합니다.
         files {
-            "%{engineSource}/*",
-            "%{engineSource}/Game/*",
-            "%{engineSource}/Manager/*",
-            "%{engineSource}/Matrix/*",
-            "%{engineSource}/Resource/*",
-            "%{engineSource}/Shader/*",
-            "%{engineSource}/Utils/*",
-            "%{engineSource}/Vector/*",
+            "%{source}/*",
+            "%{source}/Engine/*",
+            "%{source}/Game/*",
+            "%{source}/GameObject/*",
+            "%{source}/Manager/*",
+            "%{source}/Matrix/*",
+            "%{source}/Resource/*",
+            "%{source}/Shader/*",
+            "%{source}/Utils/*",
+            "%{source}/Vector/*",
 
-            "%{engineConfig}/*",
-            
-            "%{engineShader}/*",
+            "%{shader}/*",
 
-            "%{engineScript}/*",
+            "%{script}/*",
             
-            "%{engineThirdparty}/json/*",
-            "%{engineThirdparty}/miniaudio/*",
-            "%{engineThirdparty}/sqlite3/*",
-            "%{engineThirdparty}/stb/*",
+            "%{thirdparty}/miniaudio/*",
+            "%{thirdparty}/sqlite3/*",
+            "%{thirdparty}/stb/*",
         }
 
         -- 빌드 시 셰이더 컴파일은 비활성화합니다.
@@ -114,64 +94,13 @@ workspace "Snake3D"
             "26812", 
             "6385", 
         }
-        
-        -- 구성 요소별 설정을 수행합니다.
-        filter "configurations:Debug"
-            defines { "DEBUG" }
-            runtime  "Debug"
-            optimize "Off"
-            symbols "On"
-
-        filter "configurations:Release"
-            defines { "NDEBUG", "RELEASE" }
-            runtime "Release"
-            optimize "On"
-            symbols "On"
-
-        filter "configurations:Shipping"
-            defines { "NDEBUG", "SHIPPING" }
-            runtime "Release"
-            optimize "Full"
-            symbols "Off"
-
-    
-    -- 클라이언트 프로젝트
-    project "Client"
-        -- 클라이언트 프로젝트의 종류를 설정합니다.
-        kind "WindowedApp"
-
-        -- 프로그래밍 언어를 설정합니다.
-        language "C++"
-
-        -- C++의 표준을 설정합니다.
-        cppdialect "C++17"
-
-        -- 엔진을 연결합니다.
-        links {"Engine"}
-        
-        -- 클라이언트의 include 경로를 추가합니다.
-        includedirs {
-            "%{engineSource}", -- 엔진 소스 경로 추가 필요!
-            "%{engineThirdparty}", -- 엔진 서드 파티 라이브러리 추가 필요!
-            "%{clientSource}",
-            "%{clientConfig}",
-            "%{clientResource}",
-        }
-
-        -- 클라이언트의 file 경로를 추가합니다.
-        files {
-            "%{clientSource}/*",
-
-            "%{clientConfig}/*",
-        }
 
         -- 명령행 인수를 설정합니다.
         debugargs {
             "Crash=%{wks.location}..\\Crash\\",
-            "Shader=%{wks.location}..\\Engine\\HLSL\\",
-            "Resource=%{wks.location}..\\Client\\Resource\\",
-            "EngineConfig=%{wks.location}..\\Engine\\Config\\Config.json",
-            "ClientConfig=%{wks.location}..\\Client\\Config\\Config.json",
+            "Shader=%{wks.location}..\\HLSL\\",
+            "Resource=%{wks.location}..\\Resource\\",
+            "Config=%{wks.location}..\\Config\\",
         }
 
         -- 구성 요소별 설정을 수행합니다.
@@ -192,4 +121,3 @@ workspace "Snake3D"
             runtime "Release"
             optimize "Full"
             symbols "Off"
-
