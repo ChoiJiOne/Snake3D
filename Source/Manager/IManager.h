9ac9da8fa6198a42ff5b949caf60fc2313e266ce
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-09-12 08:21:40 +0900

    [GAME] 게임 내에서 사용될 매니저의 인터페이스 클래스 추가

---
+++ Source/Manager/IManager.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Utility/Macro.h"
+
+
+/**
+ * @brief 매니저 인터페이스 입니다.
+ *
+ * @note 접두사로 매니저라고 명명된 클래스는 반드시 이 클래스를 상속받아야 합니다.
+ */
+class IManager
+{
+public:
+	/**
+	 * @brief 매니저 인터페이스의 복사 생성자와 대입 연산자를 명시적으로 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(IManager);
+
+
+	/**
+	 * @brief 매니저 인터페이스를 해제합니다.
+	 *
+	 * @note 반드시 이 메서드를 이용해서 해제해야 합니다.
+	 */
+	virtual void Release() = 0;
+
+
+protected:
+	/**
+	 * @brief 매니저 인터페이스의 디폴트 생성자와 가상 소멸자입니다.
+	 */
+	DEFAULT_CONSTRUCTOR_AND_VIRTUAL_DESTRUCTOR(IManager);
+
+
+protected:
+	/**
+	 * @brief 매니저 인터페이스가 초기화되었는지 확인합니다.
+	 */
+	bool bIsInitialized_ = false;
+};
