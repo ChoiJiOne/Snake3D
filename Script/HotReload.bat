517a77d1e5ece2897b97eb16f550323bf069b542
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-09-12 07:16:50 +0900

    [SCRIPT] Visual Studio 솔루션 및 하위 프로젝트를 업데이트하는 배치 스크립트 추가

---
+++ Script/HotReload.bat
@@ -0,0 +1,31 @@
+@echo off
+
+SET project=%1
+SET visualstudio=%2
+SET premake5=Bin\\premake5.exe
+
+if "%project%" == "" (
+    echo project name is empty...
+    GOTO:EOF
+)
+
+if "%visualstudio%" == "" (
+    echo make a choice visual studio version vs2019 or vs2022...
+    GOTO:EOF
+)
+
+echo Start HotReload.bat script...
+echo Project name is %project%...
+
+@REM premake5 실행
+if "%visualstudio%" == "vs2019" (
+    echo Your Visual Studio version is %visualstudio%...
+) else if "%visualstudio%" == "vs2022" (
+    echo Your Visual Studio version is %visualstudio%...
+) else (
+    echo Your Visual Studio version %visualstudio% is illegal...
+    GOTO:EOF
+)
+
+echo Start premake5.exe...
+%premake5% %visualstudio%
