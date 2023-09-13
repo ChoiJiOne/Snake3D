c229131b2c9b9ef33c8376da5bff2cebcc9909ca
CHOIJIONE\choij <choijione02@gmail.com>
2023-09-13 12:19:02 +0900

    [GAME] 로그를 출력할 모드를 나타내는 열거형 추가

--- Source/Manager/LogManager.h
+++ Source/Manager/LogManager.h
@@ -12,6 +12,17 @@ class LogManager : public IManager
 {
 public:
 	/**
+	 * @brief 로그를 출력할 모드입니다.
+	 */
+	enum class EMode : int32_t
+	{
+		CONSOLE = 0x00,  // 기본 모드입니다.
+		WINDEBUG = 0x01, // Visual Studio 디버그 창 모드입니다.
+	};
+
+
+public:
+	/**
 	 * @brief 로그 처리를 수행하는 매니저를 싱글턴으로 설정합니다.
 	 */
 	SINGLETON(LogManager);
