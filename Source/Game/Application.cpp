e77be3ba926bdd9c28c22e855049d90f81e9bd49
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-09-13 20:00:47 +0900

    [GAME] 윈도우 창 생성 및 간단한 루프 추가

--- Source/Game/Application.cpp
+++ Source/Game/Application.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <windows.h>
 
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
 #include "Manager/LogManager.h"
 #include "Manager/RenderManager.h"
 
@@ -20,14 +23,49 @@ int32_t main(int32_t argc, char* argv[])
 	RenderManager::Get().Initialize();
 
 	{// 여기에 렌더링 기능 구현 테스트 코드 추가...
+		const int32_t width = 800;
+		const int32_t height = 600;
+		const std::string title = "Snake3D";
 
+		glfwInit();
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); 
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
+		GLFWwindow* window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+		if (window == nullptr)
+		{
+			glfwTerminate();
+			return -1;
+		}
 
+		glfwMakeContextCurrent(window);
 
+		if (!gladLoadGLLoader((GLADloadproc)(glfwGetProcAddress)))
+		{
+			glfwDestroyWindow(window);
+			window = nullptr;
 
+			glfwTerminate();
+			return -1;
+		}
 
+		while (!glfwWindowShouldClose(window))
+		{
+			if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+			{
+				glfwSetWindowShouldClose(window, true);
+			}
 
+			glfwPollEvents();
+			glfwSwapBuffers(window);
+		}
 
+		glfwDestroyWindow(window);
+		window = nullptr;
+
+		glfwTerminate();
 	}
 
 	RenderManager::Get().Release();
