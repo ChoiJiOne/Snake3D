08c90aab1de7d75d1ebd46e4c39448a541fe28be
CHOIJIONE\choij <choijione02@gmail.com>
2023-09-13 15:28:55 +0900

    [GAME] 로그 매니저와 렌더 매니저의 초기화/해제를 수행하는 기능 추가

--- Source/Game/Application.cpp
+++ Source/Game/Application.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <windows.h>
 
+#include "Manager/LogManager.h"
+#include "Manager/RenderManager.h"
+
+#include "Utility/Assertion.h"
 #include "Utility/CommandLine.h"
 #include "Utility/String.h"
+#include "Utility/Logging.h"
 #include "Utility/Macro.h"
 #include "Utility/MinidumpWriter.h"
 
@@ -10,7 +15,13 @@ int32_t main(int32_t argc, char* argv[])
 {
 	CommandLine::Parse(argc, argv);
 	MinidumpWriter::RegisterUnhandledExceptionFilter();
-	
+
+	LogManager::Get().Initialize();
+	RenderManager::Get().Initialize();
+
+	RenderManager::Get().Release();
+	LogManager::Get().Release();
+
 	MinidumpWriter::UnregisterUnhandledExceptionFilter();
 	return 0;
 }
