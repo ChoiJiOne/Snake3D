4afc15f5b5e802c9b76266a49c65855756e9c1ad
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-09-14 11:11:30 +0900

    [GAME] OpenGL을 초기화하고 컨텍스트를 생성하는 기능 추가

--- Source/Manager/RenderManager.cpp
+++ Source/Manager/RenderManager.cpp
@@ -2,16 +2,37 @@
 
 #include "Utility/Assertion.h"
 
-void RenderManager::Initialize()
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
+void RenderManager::Initialize(GLFWwindow* renderTargetWindow, bool bEnableVsync)
 {
 	ASSERT(!bIsInitialized_, "already initialize render manager...");
+	ASSERT(renderTargetWindow, "render target window is nullptr");
 
+	INFO_LOG("vsync mode %s", bEnableVsync ? "enable" : "disable");
+
+	renderTargetWindow_ = renderTargetWindow;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, majorVersion_);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, minorVersion_);
+	INFO_LOG("OpenGL version is %d.%d", majorVersion_, minorVersion_);
+	
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	INFO_LOG("OpenGL mode is core profile mode...");
+
+	glfwMakeContextCurrent(renderTargetWindow_);
+
+	ASSERT(gladLoadGLLoader((GLADloadproc)(glfwGetProcAddress)), "failed to initialize OpenGL function loader...");
+	
 	bIsInitialized_ = true;
 }
 
 void RenderManager::Release()
 {
 	ASSERT(bIsInitialized_, "you have to call Initialize method...");
+
+	renderTargetWindow_ = nullptr;
 
 	bIsInitialized_ = false;
 }
--- Source/Manager/RenderManager.h
+++ Source/Manager/RenderManager.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <cstdint>
+
 #include "Manager/IManager.h"
+
+typedef struct GLFWwindow GLFWwindow; // GLFWwindow를 사용하기 위한 전방 선언입니다.
 
 
 /**
@@ -19,8 +23,11 @@ public:
 
 	/**
 	 * @brief 렌더링 처리를 수행하는 매니저를 초기화합니다.
+	 * 
+	 * @param renderTargetWindow 렌더링 대상이 되는 윈도우입니다.
+	 * @param bEnableVsync 수직 동기화 활성화 여부입니다.
 	 */
-	void Initialize();
+	void Initialize(GLFWwindow* renderTargetWindow, bool bEnableVsync = false);
 
 
 	/**
@@ -34,4 +41,29 @@ private:
 	 * @brief 렌더링 처리를 수행하는 매니저의 디폴트 생성자와 가상 소멸자를 추가합니다.
 	 */
 	DEFAULT_CONSTRUCTOR_AND_VIRTUAL_DESTRUCTOR(RenderManager);
+
+
+private:
+	/**
+	 * @brief 렌더링 대상이 되는 윈도우입니다.
+	 */
+	GLFWwindow* renderTargetWindow_ = nullptr;
+
+
+	/**
+	 * @brief 수직 동기화 활성화 여부입니다.
+	 */
+	bool bEnableVsync_ = true;
+
+
+	/**
+	 * @brief OpenGL의 주 버전입니다.
+	 */
+	const int32_t majorVersion_ = 4;
+
+
+	/**
+	 * @brief OpenGL의 부 버전입니다.
+	 */
+	const int32_t minorVersion_ = 5;
 };
