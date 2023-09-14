a3618c75544a85499e7aa4ea9a7377c39fcb3445
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-09-14 10:25:27 +0900

    [GAME] 리소스를 관리하는 리소스 매니저 추가

---
+++ Source/Manager/ResourceManager.cpp
@@ -0,0 +1,34 @@
+#include "Manager/ResourceManager.h"
+
+#include "Utility/Assertion.h"
+
+void ResourceManager::Initialize()
+{
+	ASSERT(!bIsInitialized_, "already initialize resource manager...");
+
+	resources_ = std::unordered_map<std::string, std::unique_ptr<IResource>>();
+
+	bIsInitialized_ = true;
+}
+
+void ResourceManager::Release()
+{
+	ASSERT(bIsInitialized_, "you have to call Initialize method...");
+
+	for (auto& resource : resources_)
+	{
+		resource.second->Release();
+		resource.second.reset();
+	}
+
+	bIsInitialized_ = false;
+}
+
+void ResourceManager::RemoveResource(const std::string& signature)
+{
+	if (resources_.find(signature) != resources_.end())
+	{
+		resources_.at(signature).get()->Release();
+		resources_.erase(signature);
+	}
+}
