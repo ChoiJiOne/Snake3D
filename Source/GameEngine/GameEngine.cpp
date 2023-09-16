4c257212e83e258e91dafd83764d7f4862fd8113
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-09-16 22:42:51 +0900

    [GAME] 전체적인 게임의 초기화/해제를 수행하는 게임 엔진 추가

---
+++ Source/GameEngine/GameEngine.cpp
@@ -0,0 +1,60 @@
+#include "GameEngine/GameEngine.h"
+
+#include <glfw/glfw3.h>
+
+bool GameEngine::bIsPreInitialized_ = false;
+bool GameEngine::bIsPostInitialized_ = false;
+Window* GameEngine::mainWindow_ = nullptr;
+
+std::array<IManager*, 4> managers_ = 
+{
+	&ResourceManager::Get(),
+	&ObjectManager::Get(),
+	&RenderManager::Get(),
+	&LogManager::Get(),
+};
+
+void GameEngine::PreInitialize(int32_t argc, char* argv[])
+{
+	CommandLine::Parse(argc, argv);
+	MinidumpWriter::RegisterUnhandledExceptionFilter();
+
+	ASSERT(glfwInit(), "failed to initialize GLFW...");
+}
+
+void GameEngine::PostInitialize(Window* mainWindow)
+{
+	ASSERT(mainWindow != nullptr, "failed to create main window or not create main window...");
+	mainWindow_ = mainWindow;
+
+	LogManager::Get().Initialize();
+	RenderManager::Get().Initialize(mainWindow);
+	ObjectManager::Get().Initialize();
+	ResourceManager::Get().Initialize();
+
+	std::string vsync = CommandLine::GetValue("Vsync");
+	std::transform(vsync.begin(), vsync.end(), vsync.begin(), std::tolower);
+	RenderManager::Get().SetVsyncMode(!vsync.compare("on") ? true : false);
+
+	if(vsync.compare("on") && vsync.compare("off")) // 둘 다 만족하지 않을 경우...
+	{
+		WARN_LOG("set vsync default mode...");
+		WARN_LOG("default mode is disable vsync...");
+	}
+}
+
+void GameEngine::Release()
+{
+	for (auto& manager : managers_)
+	{
+		if (manager->IsInitialized())
+		{
+			manager->Release();
+		}
+	}
+
+	mainWindow_->Destroy();
+
+	glfwTerminate();
+	MinidumpWriter::UnregisterUnhandledExceptionFilter();
+}
---
+++ Source/GameEngine/GameEngine.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <array>
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <windows.h>
+
+#include "Manager/LogManager.h"
+#include "Manager/ObjectManager.h"
+#include "Manager/RenderManager.h"
+#include "Manager/ResourceManager.h"
+
+#include "Resource/Mesh.h"
+#include "Resource/Shader.h"
+
+#include "Utility/Assertion.h"
+#include "Utility/CommandLine.h"
+#include "Utility/FileSystem.h"
+#include "Utility/GameTimer.h"
+#include "Utility/GeometryGenerator.h"
+#include "Utility/Logging.h"
+#include "Utility/Macro.h"
+#include "Utility/MinidumpWriter.h"
+#include "Utility/String.h"
+#include "Utility/Vertex.h"
+#include "Utility/Window.h"
+
+
+/**
+ * @brief 전체적인 게임을 초기화/해제하는 엔진입니다.
+ * 
+ * @note 이 클래스의 모든 멤버 변수와 메서드는 정적(static)입니다.
+ */
+class GameEngine
+{
+public:
+	/**
+	 * @brief 게임 엔진의 본격적인 초기화 전에 호출합니다.
+	 * 
+	 * @param argc 명령행 인자의 수입니다.
+	 * @param argv 명령행 인자입니다.
+	 */
+	static void PreInitialize(int32_t argc, char* argv[]);
+
+
+	/**
+	 * @brief 게임 엔진의 초기화를 수행합니다.
+	 * 
+	 * @param mainWindow 게임의 메인 윈도우입니다.
+	 */
+	static void PostInitialize(Window* mainWindow);
+
+
+	/**
+	 * @brief 게임 엔진의 초기화를 해제합니다.
+	 */
+	static void Release();
+
+	
+	/**
+	 * @brief 메인 윈도우의 포인터를 얻습니다.
+	 * 
+	 * @return 메인 윈도우의 포인터를 반환합니다.
+	 */
+	static Window* GetMainWindowPtr() { return mainWindow_; }
+
+
+private:
+	/**
+	 * @brief 사전 초기화가 수행되었는지 확인합니다.
+	 */
+	static bool bIsPreInitialized_;
+
+
+	/**
+	 * @brief 사전 초기화 이후의 초기화가 진행되었는지 확인합니다.
+	 */
+	static bool bIsPostInitialized_;
+
+
+	/**
+	 * @brief 게임의 메인 윈도우입니다.
+	 */
+	static Window* mainWindow_;
+};
