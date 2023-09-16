da1adabecf6713fe59ae156a518bb25dd2babafd
CHOIJIONE\choij <choijione02@gmail.com>
2023-09-16 13:11:14 +0900

    [GAME] 위치, 노말(법선) 벡터, 텍스처 위치 정보를 가진 정점 추가

--- Source/Utility/Vertex.h
+++ Source/Utility/Vertex.h
@@ -454,3 +454,135 @@ struct VertexPositionNormal
 	 */
 	glm::vec3 normal_;
 };
+
+
+/**
+ * @brief 위치, 노말(법선) 벡터, 텍스처 위치 정보를 가진 정점입니다.
+ */
+struct VertexPositionNormalTexture
+{
+	/**
+	 * @brief 위치, 노말(법선) 벡터, 텍스처 위치 정보를 가진 정점의 기본 생성자입니다.
+	 */
+	VertexPositionNormalTexture()
+		: position_(0.0f, 0.0f, 0.0f), normal_(0.0f, 0.0f, 0.0f), textureCoordinate_(0.0f, 0.0f) {}
+
+
+	/**
+	 * @brief 위치, 노말(법선) 벡터, 텍스처 위치 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param position 정점의 위치 정보입니다.
+	 * @param normal 정점의 노말(법선) 정보 벡터입니다.
+	 * @param textureCoordinate 정점의 텍스처 위치 정보입니다.
+	 */
+	VertexPositionNormalTexture(const glm::vec3& position, const glm::vec3& normal, const glm::vec2& textureCoordinate)
+		: position_(position), normal_(normal), textureCoordinate_(textureCoordinate) {}
+
+
+	/**
+	 * @brief 위치, 노말(법선) 벡터, 텍스처 위치 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param px 정점 위치의 x값입니다.
+	 * @param py 정점 위치의 y값입니다.
+	 * @param pz 정점 위치의 z값입니다.
+	 * @param nx 정점 노말(법선) 벡터의 x값입니다.
+	 * @param ny 정점 노말(법선) 벡터의 y값입니다.
+	 * @param nz 정점 노말(법선) 벡터의 z값입니다.
+	 * @param s 텍스처 위치의 s값입니다.
+	 * @param t 텍스처 위치의 t값입니다.
+	 */
+	VertexPositionNormalTexture(
+		float px, float py, float pz,
+		float nx, float ny, float nz,
+		float s, float t
+	) : position_(px, py, pz), normal_(nx, ny, nz), textureCoordinate_(s, t) {}
+
+
+	/**
+	 * @brief 위치, 노말(법선) 벡터, 텍스처 위치 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param instance 복사할 정점의 인스턴스입니다.
+	 */
+	VertexPositionNormalTexture(VertexPositionNormalTexture&& instance) noexcept
+		: position_(instance.position_),
+		normal_(instance.normal_),
+		textureCoordinate_(instance.textureCoordinate_) {}
+
+
+	/**
+	 * @brief 위치, 노말(법선) 벡터, 텍스처 위치 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param instance 복사할 정점의 인스턴스입니다.
+	 */
+	VertexPositionNormalTexture(const VertexPositionNormalTexture& instance) noexcept
+		: position_(instance.position_),
+		normal_(instance.normal_),
+		textureCoordinate_(instance.textureCoordinate_) {}
+
+
+	/**
+	 * @brief 위치, 노말(법선) 벡터, 텍스처 위치 정보를 가진 정점의 대입 연산자입니다.
+	 *
+	 * @param instance 대입할 정점의 인스턴스입니다.
+	 *
+	 * @return 대입한 객체의 참조자를 반환합니다.
+	 */
+	VertexPositionNormalTexture& operator=(VertexPositionNormalTexture&& instance) noexcept
+	{
+		if (this == &instance) return *this;
+
+		position_ = instance.position_;
+		normal_ = instance.normal_;
+		textureCoordinate_ = instance.textureCoordinate_;
+
+		return *this;
+	}
+
+
+	/**
+	 * @brief 위치, 노말(법선) 벡터, 텍스처 위치 정보를 가진 정점의 대입 연산자입니다.
+	 *
+	 * @param instance 대입할 정점의 인스턴스입니다.
+	 *
+	 * @return 대입한 객체의 참조자를 반환합니다.
+	 */
+	VertexPositionNormalTexture& operator=(const VertexPositionNormalTexture& instance) noexcept
+	{
+		if (this == &instance) return *this;
+
+		position_ = instance.position_;
+		normal_ = instance.normal_;
+		textureCoordinate_ = instance.textureCoordinate_;
+
+		return *this;
+	}
+
+
+	/**
+	 * @brief 정점의 바이트 보폭 값을 얻습니다.
+	 *
+	 * @return 정점의 바이트 보폭(stride) 값을 반환합니다.
+	 */
+	static uint32_t GetStride()
+	{
+		return sizeof(VertexPositionNormalTexture);
+	}
+
+
+	/**
+	 * @brief 정점의 위치입니다.
+	 */
+	glm::vec3 position_;
+
+
+	/**
+	 * @brief 노말(법선) 벡터입니다.
+	 */
+	glm::vec3 normal_;
+
+
+	/**
+	 * @brief 텍스처의 위치입니다.
+	 */
+	glm::vec2 textureCoordinate_;
+};
