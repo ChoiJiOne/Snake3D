e24b391faa6ed01512161339fe91759e97a06df5
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-09-18 08:55:17 +0900

    [GAME] 점광 (Point Light) 구현 테스트 코드 추가

--- Source/Game/Application.cpp
+++ Source/Game/Application.cpp
@@ -16,10 +16,13 @@ int32_t main(int32_t argc, char* argv[])
 
 	std::vector<VertexPositionNormal> vertices;
 	std::vector<uint32_t> indices;
-	GeometryGenerator::CreateSphere(1.0f, 50, 50, vertices, indices);
+	GeometryGenerator::CreateSphere(0.5f, 50, 50, vertices, indices);
 
-	Shader* shader = ResourceManager::Get().AddResource<Shader>("Shader");
-	shader->Initialize("shader.vert", "shader.frag");
+	Shader* shader = ResourceManager::Get().AddResource<Shader>("DirectionalLight");
+	shader->Initialize("DirectionalLight.vert", "DirectionalLight.frag");
+
+	shader = ResourceManager::Get().AddResource<Shader>("PointLight");
+	shader->Initialize("PointLight.vert", "PointLight.frag");
 
 	Mesh* mesh = ResourceManager::Get().AddResource<Mesh>("Sphere");
 	mesh->Initialize(vertices, indices);
@@ -30,12 +33,20 @@ int32_t main(int32_t argc, char* argv[])
 	Model* model = ResourceManager::Get().AddResource<Model>("Model");
 	model->Initialize(mesh, material);
 
-	Light* light = ObjectManager::Get().AddGameObject<Light>("Light");
-	light->Initialize(glm::vec3(5.0f, 5.0f, 5.0f), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
+	PointLight* light = ObjectManager::Get().AddGameObject<PointLight>("PointLight");
+	light->Initialize(
+		glm::vec3(4.0f, 2.0f, 2.0f),
+		glm::vec4(0.2f, 0.2f, 0.2f, 1.0f),
+		glm::vec4(0.5f, 0.5f, 0.5f, 1.0f),
+		glm::vec4(1.0f, 1.0f, 1.0f, 1.0f),
+		1.0f,
+		0.09f,
+		0.032f
+	);
 
 	Camera3D* camera = ObjectManager::Get().AddGameObject<Camera3D>("Camera");
 	camera->Initialize(
-		glm::vec3(0.0f, 0.0f, 5.0f),
+		glm::vec3(0.0f, 0.0f, 10.0f),
 		glm::vec3(0.0f, 0.0f, 0.0f),
 		glm::vec3(0.0f, 1.0f, 0.0f),
 		45.0f,
@@ -56,12 +67,21 @@ int32_t main(int32_t argc, char* argv[])
 		RenderManager::Get().SetRenderTargetWindowViewport();
 
 		light->SetPosition(glm::vec3(
-			5.0f * sinf(static_cast<float>(glfwGetTime())),
-			0.0f,
-			5.0f * cosf(static_cast<float>(glfwGetTime()))
+			4.0f * sinf(static_cast<float>(glfwGetTime())), 
+			2.0f, 
+			2.0f
 		));
 
-		RenderManager::Get().RenderModel3D(glm::mat4(1.0f), camera, model, light);
+		glm::translate(glm::mat4(1.0f), glm::vec3(-4.0f, 0.0f, 0.0f));
+		RenderManager::Get().RenderModel3D(glm::translate(glm::mat4(1.0f), glm::vec3(-4.0f, 0.0f, 0.0f)), camera, model, light);
+		RenderManager::Get().RenderModel3D(glm::translate(glm::mat4(1.0f), glm::vec3(-3.0f, 0.0f, 0.0f)), camera, model, light);
+		RenderManager::Get().RenderModel3D(glm::translate(glm::mat4(1.0f), glm::vec3(-2.0f, 0.0f, 0.0f)), camera, model, light);
+		RenderManager::Get().RenderModel3D(glm::translate(glm::mat4(1.0f), glm::vec3(-1.0f, 0.0f, 0.0f)), camera, model, light);
+		RenderManager::Get().RenderModel3D(glm::translate(glm::mat4(1.0f), glm::vec3(+0.0f, 0.0f, 0.0f)), camera, model, light);
+		RenderManager::Get().RenderModel3D(glm::translate(glm::mat4(1.0f), glm::vec3(+1.0f, 0.0f, 0.0f)), camera, model, light);
+		RenderManager::Get().RenderModel3D(glm::translate(glm::mat4(1.0f), glm::vec3(+2.0f, 0.0f, 0.0f)), camera, model, light);
+		RenderManager::Get().RenderModel3D(glm::translate(glm::mat4(1.0f), glm::vec3(+3.0f, 0.0f, 0.0f)), camera, model, light);
+		RenderManager::Get().RenderModel3D(glm::translate(glm::mat4(1.0f), glm::vec3(+4.0f, 0.0f, 0.0f)), camera, model, light);
 		
 		RenderManager::Get().EndFrame();
 	}
