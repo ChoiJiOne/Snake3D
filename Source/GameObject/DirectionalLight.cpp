bf661c955b9f97600aaaac93a5dca50d1fab0d2c
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-09-18 08:13:00 +0900

    [GAME] 직선광 오브젝트 추가

---
+++ Source/GameObject/DirectionalLight.cpp
@@ -0,0 +1,41 @@
+#include "DirectionalLight.h"
+
+#include "Utility/Assertion.h"
+
+DirectionalLight::~DirectionalLight()
+{
+	if (bIsInitialized_)
+	{
+		Release();
+	}
+}
+
+void DirectionalLight::Initialize(const glm::vec4& ambient, const glm::vec4& diffuse, const glm::vec4& specular, const glm::vec3& direction)
+{
+	ASSERT(!bIsInitialized_, "already initialize light object...");
+
+	ambient_ = ambient;
+	diffuse_ = diffuse;
+	specular_ = specular;
+	direction_ = direction;
+
+	type_ = Light::EType::DirectionalLight;
+
+	bIsInitialized_ = true;
+}
+
+void DirectionalLight::Update(float deltaSeconds)
+{
+	// 여기서는 아무 동작도 수행하지 않습니다.
+}
+
+void DirectionalLight::Render()
+{
+	// 여기서는 아무 동작도 수행하지 않습니다.
+}
+
+void DirectionalLight::Release()
+{
+	ASSERT(bIsInitialized_, "you have to call Initialize method...");
+	bIsInitialized_ = false;
+}
