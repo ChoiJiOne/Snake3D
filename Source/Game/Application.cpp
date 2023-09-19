120571cb0f93c5a5ad8dbaa36fc7c5efbdcc0f4a
CHOIJIONE\choij <choijione02@gmail.com>
2023-09-19 18:34:50 +0900

    [GAME] 큐브맵 렌더링 테스트 및 전체적인 코드 정리

--- Source/Game/Application.cpp
+++ Source/Game/Application.cpp
@@ -14,53 +14,83 @@ int32_t main(int32_t argc, char* argv[])
 
 	GameEngine::PostInitialize(&window);
 
-	std::vector<VertexPositionNormal> vertices;
-	std::vector<uint32_t> indices;
-	GeometryGenerator::CreateSphere(0.5f, 50, 50, vertices, indices);
+	{ // 셰이더 초기화...
+		Shader* shader = nullptr;
 
-	Shader* shader = ResourceManager::Get().AddResource<Shader>("DirectionalLight");
-	shader->Initialize("DirectionalLight.vert", "DirectionalLight.frag");
+		shader = ResourceManager::Get().AddResource<Shader>("DirectionalLight");
+		shader->Initialize("DirectionalLight.vert", "DirectionalLight.frag");
 
-	shader = ResourceManager::Get().AddResource<Shader>("SpotLight");
-	shader->Initialize("SpotLight.vert", "SpotLight.frag");	
-	
-	shader = ResourceManager::Get().AddResource<Shader>("PostProcessing");
-	shader->Initialize("PostProcessing.vert", "PostProcessing.frag");	
-	
-	Mesh* mesh = ResourceManager::Get().AddResource<Mesh>("Sphere");
-	mesh->Initialize(vertices, indices);
+		shader = ResourceManager::Get().AddResource<Shader>("PointLight");
+		shader->Initialize("PointLight.vert", "PointLight.frag");
 
-	Material* material = ResourceManager::Get().AddResource<Material>("Material");
-	material->Initialize(glm::vec3(0.2f, 0.2f, 0.2f), glm::vec3(1.0f, 0.5f, 0.31f), glm::vec3(1.0f, 1.0f, 1.0f), 32.0f);
+		shader = ResourceManager::Get().AddResource<Shader>("SpotLight");
+		shader->Initialize("SpotLight.vert", "SpotLight.frag");
 
-	Model* model = ResourceManager::Get().AddResource<Model>("Model");
-	model->Initialize(mesh, material);
+		shader = ResourceManager::Get().AddResource<Shader>("PostProcessing");
+		shader->Initialize("PostProcessing.vert", "PostProcessing.frag");		
+		
+		shader = ResourceManager::Get().AddResource<Shader>("Skybox");
+		shader->Initialize("Skybox.vert", "Skybox.frag");
+	}
 
-	SpotLight* light = ObjectManager::Get().AddGameObject<SpotLight>("SpotLight");
-	light->Initialize(
-		glm::vec3(0.0f, 0.0f, 0.0f),
-		glm::vec3(0.0f, -1.0f, 0.0f),
-		glm::cos(glm::radians(12.5f)),
-		glm::cos(glm::radians(45.0f)),
-		glm::vec3(0.5f, 0.5f, 0.5f),
-		glm::vec3(0.7f, 0.7f, 0.7f),
-		glm::vec3(1.0f, 1.0f, 1.0f),
-		1.0f,
-		0.09f,
-		0.032f
-	);
+	{ // 리소스 생성...
+		std::vector<VertexPositionNormal> vertices;
+		std::vector<uint32_t> indices;
+		GeometryGenerator::CreateSphere(0.5f, 50, 50, vertices, indices);
 
-	Camera3D* camera = ObjectManager::Get().AddGameObject<Camera3D>("Camera");
-	camera->Initialize(
-		glm::vec3(0.0f, 0.0f, 10.0f),
-		glm::vec3(0.0f, 0.0f, 0.0f),
-		glm::vec3(0.0f, 1.0f, 0.0f),
-		45.0f,
-		RenderManager::Get().GetRenderTargetWindowAspectRatio(),
-		0.1f,
-		100.0f
-	);
-	
+		Mesh* mesh = ResourceManager::Get().AddResource<Mesh>("Sphere");
+		mesh->Initialize(vertices, indices);
+
+		Material* material = ResourceManager::Get().AddResource<Material>("Material");
+		material->Initialize(glm::vec3(0.2f, 0.2f, 0.2f), glm::vec3(1.0f, 0.5f, 0.31f), glm::vec3(1.0f, 1.0f, 1.0f), 32.0f);
+
+		Model* model = ResourceManager::Get().AddResource<Model>("Model");
+		model->Initialize(mesh, material);
+
+		std::string resourcePath = CommandLine::GetValue("Resource");
+		CubeMap* cubeMap = ResourceManager::Get().AddResource<CubeMap>("CubeMap");
+		cubeMap->Initialize(
+			resourcePath + "Skybox\\right.png",
+			resourcePath + "Skybox\\left.png",
+			resourcePath + "Skybox\\top.png",
+			resourcePath + "Skybox\\bottom.png",
+			resourcePath + "Skybox\\front.png",
+			resourcePath + "Skybox\\back.png"
+		);
+	}
+
+	{ // 게임 오브젝트 생성...
+		SpotLight* light = ObjectManager::Get().AddGameObject<SpotLight>("SpotLight");
+		light->Initialize(
+			glm::vec3(0.0f, 0.0f, 0.0f),
+			glm::vec3(0.0f, -1.0f, 0.0f),
+			glm::cos(glm::radians(12.5f)),
+			glm::cos(glm::radians(45.0f)),
+			glm::vec3(0.5f, 0.5f, 0.5f),
+			glm::vec3(0.7f, 0.7f, 0.7f),
+			glm::vec3(1.0f, 1.0f, 1.0f),
+			1.0f,
+			0.09f,
+			0.032f
+		);
+
+		Camera3D* camera = ObjectManager::Get().AddGameObject<Camera3D>("Camera");
+		camera->Initialize(
+			glm::vec3(0.0f, 0.0f, 10.0f),
+			glm::vec3(0.0f, 0.0f, 0.0f),
+			glm::vec3(0.0f, 1.0f, 0.0f),
+			45.0f,
+			RenderManager::Get().GetRenderTargetWindowAspectRatio(),
+			0.1f,
+			100.0f
+		);
+	}
+
+	CubeMap* cubeMap = ResourceManager::Get().GetResource<CubeMap>("CubeMap");
+	Camera3D* camera = ObjectManager::Get().GetGameObject<Camera3D>("Camera");
+	Model* model = ResourceManager::Get().GetResource<Model>("Model");
+	SpotLight* light = ObjectManager::Get().GetGameObject<SpotLight>("SpotLight");
+
 	while (!glfwWindowShouldClose(window.GetWindowPtr()))
 	{
 		glfwPollEvents();
@@ -72,30 +102,20 @@ int32_t main(int32_t argc, char* argv[])
 		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 		RenderManager::Get().SetRenderTargetWindowViewport();
 
-		glm::vec3 position = glm::vec3(
-			4.0f * sinf(static_cast<float>(glfwGetTime())),
-			2.0f,
-			0.0f
-		);
+		camera->SetEyePosition(glm::vec3(
+			10.0f * cosf(static_cast<float>(glfwGetTime()) / 10.0f),
+			0.0f,
+			10.0f * sinf(static_cast<float>(glfwGetTime()) / 10.0f)
+		));
+		light->SetPosition(glm::vec3(2.0f * sinf(static_cast<float>(glfwGetTime())), 2.0f, 0.0f));
 
-		light->SetPosition(position);
-		//light->SetDirection(-glm::normalize(position));
-
-		RenderManager::Get().RenderModel3D(glm::translate(glm::mat4(1.0f), glm::vec3(-4.0f, 0.0f, 0.0f)), camera, model, light);
-		RenderManager::Get().RenderModel3D(glm::translate(glm::mat4(1.0f), glm::vec3(-3.0f, 0.0f, 0.0f)), camera, model, light);
+		RenderManager::Get().RenderCubeMap(camera, cubeMap);
 		RenderManager::Get().RenderModel3D(glm::translate(glm::mat4(1.0f), glm::vec3(-2.0f, 0.0f, 0.0f)), camera, model, light);
 		RenderManager::Get().RenderModel3D(glm::translate(glm::mat4(1.0f), glm::vec3(-1.0f, 0.0f, 0.0f)), camera, model, light);
 		RenderManager::Get().RenderModel3D(glm::translate(glm::mat4(1.0f), glm::vec3(+0.0f, 0.0f, 0.0f)), camera, model, light);
 		RenderManager::Get().RenderModel3D(glm::translate(glm::mat4(1.0f), glm::vec3(+1.0f, 0.0f, 0.0f)), camera, model, light);
 		RenderManager::Get().RenderModel3D(glm::translate(glm::mat4(1.0f), glm::vec3(+2.0f, 0.0f, 0.0f)), camera, model, light);
-		RenderManager::Get().RenderModel3D(glm::translate(glm::mat4(1.0f), glm::vec3(+3.0f, 0.0f, 0.0f)), camera, model, light);
-		RenderManager::Get().RenderModel3D(glm::translate(glm::mat4(1.0f), glm::vec3(+4.0f, 0.0f, 0.0f)), camera, model, light);
-
-		//RenderManager::Get().BlurEffect(200.0f);
-		//RenderManager::Get().ColorEffect(0.5f, 0.5f, 1.0f);
-		//RenderManager::Get().InversionEffect();
-		//RenderManager::Get().GrayScaleEffect();
-
+		
 		RenderManager::Get().EndFrame();
 	}
 
