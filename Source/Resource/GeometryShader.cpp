4a80bfcbf4b63f8ce4b6b2b06029c45cedbe267d
CHOIJIONE\choij <choijione02@gmail.com>
2023-09-21 10:33:22 +0900

    [GAME] 정점마다 색상이 다른 2D 와이어 프레임 삼각형을 렌더링하는 기능 추가

--- Source/Resource/GeometryShader.cpp
+++ Source/Resource/GeometryShader.cpp
@@ -156,3 +156,26 @@ void GeometryShader::DrawWireframeTriangle2D(const glm::mat4& projection, const 
 
 	Shader::Unbind();
 }
+
+void GeometryShader::DrawWireframeTriangle2D(const glm::mat4& projection, const glm::vec2& fromPosition, const glm::vec4& fromColor, const glm::vec2& byPosition, const glm::vec4& byColor, const glm::vec2& toPosition, const glm::vec4& toColor)
+{
+	vertices_[0] = VertexPositionColor(glm::vec3(fromPosition.x + 0.5f, fromPosition.y + 0.5f, 0.0f), fromColor);
+	vertices_[1] = VertexPositionColor(glm::vec3(  byPosition.x + 0.5f,   byPosition.y + 0.5f, 0.0f),   byColor);
+	vertices_[2] = VertexPositionColor(glm::vec3(  toPosition.x + 0.5f,   toPosition.y + 0.5f, 0.0f),   toColor);
+	vertices_[3] = VertexPositionColor(glm::vec3(fromPosition.x + 0.5f, fromPosition.y + 0.5f, 0.0f), fromColor);
+
+	void* bufferPtr = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+	std::memcpy(bufferPtr, reinterpret_cast<const void*>(&vertices_[0]), VertexPositionColor::GetStride() * vertices_.size());
+	glUnmapBuffer(GL_ARRAY_BUFFER);
+
+	Shader::Bind();
+
+	Shader::SetMat4Parameter("view", glm::mat4(1.0f));
+	Shader::SetMat4Parameter("projection", projection);
+
+	glBindVertexArray(vertexArrayObject_);
+	glDrawArrays(GL_LINE_STRIP, 0, 4);
+	glBindVertexArray(0);
+
+	Shader::Unbind();
+}
