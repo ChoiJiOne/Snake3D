65fc820908ff1374e00dcc1480692cf3a7c791dd
CHOIJIONE\choij <choijione02@gmail.com>
2023-09-24 13:08:04 +0900

    [GAME] 게임 타이머와 업데이트 및 렌더링 가능한 오브젝트 목록 추가
    - 오브젝트 목록을 기반으로 일괄적으로 업데이트 및 렌더링을 수행하도록 구현할 예정

--- Source/Game/Application.cpp
+++ Source/Game/Application.cpp
@@ -10,17 +10,34 @@ int32_t main(int32_t argc, char* argv[])
 	window.Create("Snake3D", 800, 600);
 
 	GameEngine::PostInitialize(&window);
+
+	GameTimer gameTimer;
+	std::vector<IGameObject*> updateObjects;
+	std::vector<IGameObject*> renderObjects;
 	
+	gameTimer.Reset();
 	while (!glfwWindowShouldClose(window.GetWindowPtr()))
 	{
+		gameTimer.Tick();
+
 		glfwPollEvents();
 		if (glfwGetKey(window.GetWindowPtr(), GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		{
 			glfwSetWindowShouldClose(window.GetWindowPtr(), true);
 		}
 
+		for (auto& updateObject : updateObjects)
+		{
+			updateObject->Update(gameTimer.GetDeltaSeconds());
+		}
+
 		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 		RenderManager::Get().SetRenderTargetWindowViewport();
+
+		for (auto& renderObject : renderObjects)
+		{
+			renderObject->Render();
+		}
 
 		RenderManager::Get().EndFrame();
 	}
