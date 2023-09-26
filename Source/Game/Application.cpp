c0bd4feb87821dc39f0de9b83d7abdedd329ab74
CHOIJIONE\choij <choijione02@gmail.com>
2023-09-26 14:01:20 +0900

    [GAME] 뱀 오브젝트 추가 및 라이팅과 카메라 위치 조정

--- Source/Game/Application.cpp
+++ Source/Game/Application.cpp
@@ -1,5 +1,6 @@
 #include "Game/Grid.h"
 #include "Game/MovableCamera.h"
+#include "Game/Snake.h"
 #include "Game/SpaceBackground.h"
 
 #include "GameEngine/GameEngine.h"
@@ -14,7 +15,7 @@ int32_t main(int32_t argc, char* argv[])
 	GameEngine::PostInitialize(&window);
 
 	MovableCamera* camera = ObjectManager::Get().AddGameObject<MovableCamera>("Camera");
-	camera->Initialize(glm::vec3(0.0f, 20.0f, 30.0f), 45.0f, RenderManager::Get().GetRenderTargetWindowAspectRatio(), 0.1f, 100.0f);
+	camera->Initialize(glm::vec3(0.0f, 15.0f, 30.0f), 45.0f, RenderManager::Get().GetRenderTargetWindowAspectRatio(), 0.1f, 100.0f);
 
 	SpaceBackground* background = ObjectManager::Get().AddGameObject<SpaceBackground>("Background");
 	background->Initialize();
@@ -24,7 +25,7 @@ int32_t main(int32_t argc, char* argv[])
 
 	SpotLight* light = ObjectManager::Get().AddGameObject<SpotLight>("GlobalLight");
 	light->Initialize(
-		glm::vec3(0.0f, 20.0f, 0.0f), 
+		glm::vec3(0.0f, 5.0f, 0.0f), 
 		glm::vec3(0.0f, -1.0f, 0.0f),
 		glm::cos(glm::radians(12.0f)),
 		glm::cos(glm::radians(45.0f)),
@@ -36,12 +37,17 @@ int32_t main(int32_t argc, char* argv[])
 		0.032f
 	);
 
+	Snake* snake = ObjectManager::Get().AddGameObject<Snake>("Snake");
+	snake->Initialize(glm::vec3(0.1f, 0.5f, 1.0f));
+
 	std::vector<IGameObject*> updateObjects = {
 		camera,
+		snake,
 	};
 	std::vector<IGameObject*> renderObjects = {
 		background,
 		grid,
+		snake,
 	};
 
 	GameTimer gameTimer;
